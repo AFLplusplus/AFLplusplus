bec9b307db299b586c2574031d3cc1a491dc00c3
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-08-27 20:57:52 +0200

    neverzero qemu for x86/x86_64

--- config.h
+++ config.h
@@ -339,6 +339,10 @@
 #define  CTEST_CORE_TRG_MS  1000
 #define  CTEST_BUSY_CYCLES  (10 * 1000 * 1000)
 
+/* Enable NeverZero counters in QEMU mode */
+
+#define AFL_QEMU_NOT_ZERO
+
 /* Uncomment this to use inferior block-coverage-based instrumentation. Note
    that you need to recompile the target binary for this to have any effect: */
 
