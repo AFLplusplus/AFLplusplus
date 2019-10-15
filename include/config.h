3dec452a8a5a21fee524c5e4bc81e680c3a1fc04
van Hauser <vh@thc.org>
2019-10-15 16:50:48 +0200

    v2.57d

--- include/config.h
+++ include/config.h
@@ -26,7 +26,7 @@
 
 /* Version string: */
 
-#define VERSION "++2.57c"  // c = release, d = volatile github dev
+#define VERSION "++2.57d"  // c = release, d = volatile github dev
 
 /******************************************************
  *                                                    *
