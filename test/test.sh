bdf4bdd480a7e2a971dd93e115b4d0970c434ebc
van Hauser <vh@thc.org>
2019-12-02 11:05:09 +0100

    increase time for radamsa test

--- test/test.sh
+++ test/test.sh
@@ -426,9 +426,9 @@ test -e ../libradamsa.so && {
   test -e test-instr.plain && {
     mkdir -p in
     echo 0 > in/in
-    $ECHO "$GREY[*] running afl-fuzz with radamsa, this will take approx 10 seconds"
+    $ECHO "$GREY[*] running afl-fuzz with radamsa, this will take approx 15 seconds"
     {
-      ../afl-fuzz -RR -V10 -m ${MEM_LIMIT} -i in -o out -- ./test-instr.plain >>errors 2>&1
+      ../afl-fuzz -RR -V15 -m ${MEM_LIMIT} -i in -o out -- ./test-instr.plain >>errors 2>&1
     } >>errors 2>&1
     test -n "$( ls out/queue/id:000002* 2> /dev/null )" && {
       $ECHO "$GREEN[+] libradamsa performs good - and very slow - mutations"
