2692ef788bd0dccff4c9b012a2fd0cf25d618228
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-01-06 16:06:35 +0100

    moar copyright notes

--- include/hash.h
+++ include/hash.h
@@ -15,6 +15,7 @@
    Other code written by Michal Zalewski
 
    Copyright 2016 Google Inc. All rights reserved.
+   Copyright 2019-2020 AFLplusplus Project. All rights reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
