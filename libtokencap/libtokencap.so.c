12df4c4af7b7bf394fa92d40c6b3509da97d556d
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-02-13 20:07:48 +0100

    fix strncmp in tokencap

--- libtokencap/libtokencap.so.c
+++ libtokencap/libtokencap.so.c
@@ -330,8 +330,8 @@ int strncmp(const char* str1, const char* str2, size_t len) {
 
     unsigned char c1 = *str1, c2 = *str2;
 
-    if (!c1) return 0;
     if (c1 != c2) return (c1 > c2) ? 1 : -1;
+    if (!c1) return 0;
     str1++;
     str2++;
 
