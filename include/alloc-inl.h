d83ab141f62c19203e818536a18eb5f4acb8780a
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-02-27 23:22:29 +0100

    cpyright

--- include/alloc-inl.h
+++ include/alloc-inl.h
@@ -1,29 +1,27 @@
 /*
-  Copyright 2013 Google LLC All rights reserved.
+   american fuzzy lop++ - error-checking, memory-zeroing alloc routines
+   --------------------------------------------------------------------
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at:
+   Originally written by Michal Zalewski
 
-    http://www.apache.org/licenses/LICENSE-2.0
+   Now maintained by Marc Heuse <mh@mh-sec.de>,
+                        Heiko Eißfeldt <heiko.eissfeldt@hexco.de> and
+                        Andrea Fioraldi <andreafioraldi@gmail.com>
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+   Copyright 2016, 2017 Google Inc. All rights reserved.
+   Copyright 2019-2020 AFLplusplus Project. All rights reserved.
 
-/*
-   american fuzzy lop - error-checking, memory-zeroing alloc routines
-   ------------------------------------------------------------------
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
 
-   Written and maintained by Michal Zalewski <lcamtuf@google.com>
+     http://www.apache.org/licenses/LICENSE-2.0
 
    This allocator is not designed to resist malicious attackers (the canaries
    are small and predictable), but provides a robust and portable way to detect
    use-after-free, off-by-one writes, stale pointers, and so on.
-*/
+
+ */
 
 #ifndef _HAVE_ALLOC_INL_H
 #define _HAVE_ALLOC_INL_H
