1136e887bdf77ed64bd3eac28bf7f86705dd2a6d
Dominik Maier <domenukk@gmail.com>
2020-03-09 21:34:11 +0100

    now wiht 90% less leaks

--- include/common.h
+++ include/common.h
@@ -34,6 +34,9 @@
 void detect_file_args(char** argv, u8* prog_in, u8 *use_stdin);
 void check_environment_vars(char** env);
 
+char **argv_cpy_dup(int argc, char **argv);
+void argv_cpy_free(char **argv);
+
 char** get_qemu_argv(u8* own_loc, u8** target_path_p, int argc, char** argv);
 char** get_wine_argv(u8* own_loc, u8** target_path_p, int argc, char** argv);
 char*  get_afl_env(char* env);
