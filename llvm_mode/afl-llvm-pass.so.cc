d1aa2c7f33f71568a1a7cca98a67a036a4737b1e
hexcoder- <heiko@hexco.de>
2020-03-28 09:31:30 +0100

    making 'CFLAGS="-m32" make source-only tests' work

--- llvm_mode/afl-llvm-pass.so.cc
+++ llvm_mode/afl-llvm-pass.so.cc
@@ -587,7 +587,7 @@ bool AFLCoverage::runOnModule(Module &M) {
     else {
 
       char modeline[100];
-      snprintf(modeline, sizeof(modeline), "%s%s%s%s",
+      snprintf(modeline, sizeof(modeline), "%s%s%s%s%s",
                getenv("AFL_HARDEN") ? "hardened" : "non-hardened",
                getenv("AFL_USE_ASAN") ? ", ASAN" : "",
                getenv("AFL_USE_MSAN") ? ", MSAN" : "",
