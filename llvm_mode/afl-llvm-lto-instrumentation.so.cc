d6d76bbf559eb22062a49d7bb4dd85b0be506d1a
hexcoder- <heiko@hexco.de>
2020-03-28 09:54:40 +0100

    llvm_mode: fix compiler warnings FORTIFY_SOURCE needs -O

--- llvm_mode/afl-llvm-lto-instrumentation.so.cc
+++ llvm_mode/afl-llvm-lto-instrumentation.so.cc
@@ -392,7 +392,7 @@ bool AFLLTOPass::runOnModule(Module &M) {
     else {
 
       char modeline[100];
-      snprintf(modeline, sizeof(modeline), "%s%s%s%s",
+      snprintf(modeline, sizeof(modeline), "%s%s%s%s%s",
                getenv("AFL_HARDEN") ? "hardened" : "non-hardened",
                getenv("AFL_USE_ASAN") ? ", ASAN" : "",
                getenv("AFL_USE_MSAN") ? ", MSAN" : "",
