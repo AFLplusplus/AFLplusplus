9adcc73d617f22ffdfaba0491374dc0b7edcf8cd
van Hauser <vh@thc.org>
2020-04-17 19:39:32 +0200

    v2.64d init

--- include/config.h
+++ include/config.h
@@ -28,7 +28,7 @@
 /* Version string: */
 
 // c = release, d = volatile github dev, e = experimental branch
-#define VERSION "++2.64c"
+#define VERSION "++2.64d"
 
 /******************************************************
  *                                                    *
