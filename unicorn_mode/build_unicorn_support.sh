755f91db92193423eeb920e504754a29d3e55d47
Assaf Carlsbad <carlsbad@sentinelone.com>
2020-05-04 02:04:51 +0300

    Fix Unicorn support build script.

--- unicorn_mode/build_unicorn_support.sh
+++ unicorn_mode/build_unicorn_support.sh
@@ -209,9 +209,9 @@ cd ../samples/simple || exit 1
 
 # Run afl-showmap on the sample application. If anything comes out then it must have worked!
 unset AFL_INST_RATIO
-echo 0 | ../../../afl-showmap -U -m none -t 2000 -q -o .test-instr0 -- $PYTHONBIN simple_test_harness.py ./sample_inputs/sample1.bin || exit 1
+echo 0 | ../../../afl-showmap -U -m none -t 2000 -q -o ./test-instr0 -- $PYTHONBIN ./simple_test_harness.py ./sample_inputs/sample1.bin || exit 1
 
-if [ -s .test-instr0 ]
+if [ -s ./test-instr0 ]
 then
 
   echo "[+] Instrumentation tests passed. "
