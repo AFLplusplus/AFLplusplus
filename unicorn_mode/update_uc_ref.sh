4ee93331dc39e7abdc27ce437fc7ac594a9099bd
Dominik Maier <domenukk@gmail.com>
2020-05-11 15:11:11 +0200

    updated unicornafl

--- unicorn_mode/update_uc_ref.sh
+++ unicorn_mode/update_uc_ref.sh
@@ -1,20 +1,40 @@
 #/bin/sh
 
+##################################################
+# AFL++ internal tool to update unicornafl ref.
+# Usage: ./update_uc_ref.sh <new commit hash>
+# If no commit hash was provided, it'll take HEAD.
+##################################################
+
 UC_VERSION_FILE='./UNICORNAFL_VERSION'
 
 NEW_VERSION="$1"
-if [ -z "$NEW_VERSION" ]; then
+
+if [ "$NEW_VERSION" = "-h" ]; then
   echo "Internal script to update bound unicornafl version."
   echo
   echo "Usage: ./update_uc_ref.sh <new commit hash>"
+  echo "If no commit hash is provided, will use HEAD."
+  echo "-h to show this help screen."
   exit 1
 fi
 
 git submodule init && git submodule update || exit 1
-cd ./unicornafl
+cd ./unicornafl || exit 1
 git fetch origin master 1>/dev/null || exit 1
 git stash 1>/dev/null 2>/dev/null
 git stash drop 1>/dev/null 2>/dev/null
+
+if [ -z "$NEW_VERSION" ]; then
+  # No version provided, take HEAD.
+  NEW_VERSION=$(git rev-parse --short HEAD)
+fi
+
+if [ -z "$NEW_VERSION" ]; then
+  echo "Error getting version."
+  exit 1
+fi
+
 git checkout "$NEW_VERSION" || exit 1
 
 cd ..
