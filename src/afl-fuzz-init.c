9fdd7c71661d65721a3792fa060f61fd859d8a62
Dominik Maier <domenukk@gmail.com>
2020-05-14 22:25:50 +0200

    unicorn mode may run scripts

--- src/afl-fuzz-init.c
+++ src/afl-fuzz-init.c
@@ -1996,7 +1996,7 @@ void check_binary(afl_state_t *afl, u8 *fname) {
 
   }
 
-  if (afl->afl_env.afl_skip_bin_check || afl->use_wine) { return; }
+  if (afl->afl_env.afl_skip_bin_check || afl->use_wine || afl->unicorn_mode) { return; }
 
   /* Check for blatant user errors. */
 
