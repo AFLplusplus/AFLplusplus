876779c677fdf5cd6d73dfd234b4615fc94f5261
Dominik Maier <domenukk@gmail.com>
2020-05-30 20:38:01 +0200

    comment

--- include/common.h
+++ include/common.h
@@ -107,14 +107,7 @@ u8 *u_stringify_mem_size(u8 *buf, u64 val);
 
 u8 *u_stringify_time_diff(u8 *buf, u64 cur_ms, u64 event_ms);
 
-/* Wrapper for select() and read(), reading exactly len bytes.
-  Returns the time passed to read.
-  stop_soon should point to a variable indicating ctrl+c was pressed.
-  If the wait times out, returns timeout_ms + 1;
-  Returns 0 if an error occurred (fd closed, signal, ...); */
-u32 read_timed(s32 fd, void *buf, size_t len, u32 timeout_ms,
-               volatile u8 *stop_soon_p);
-
+/* Reads the map size from ENV */
 u32 get_map_size(void);
 
 #endif
