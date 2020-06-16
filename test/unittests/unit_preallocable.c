c9e8923e63b96b1dec1a9fe8e7f23a3346b7f359
hexcoder- <heiko@hexco.de>
2020-06-16 22:37:56 +0200

    fix unit tests when exit is called at the end

--- test/unittests/unit_preallocable.c
+++ test/unittests/unit_preallocable.c
@@ -109,6 +109,6 @@ int main(int argc, char **argv) {
 	};
 
     //return cmocka_run_group_tests (tests, setup, teardown);
-    return cmocka_run_group_tests (tests, NULL, NULL);
+    __real_exit( cmocka_run_group_tests (tests, NULL, NULL) );
 
 }
