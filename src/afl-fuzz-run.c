a49b5ef072011cc840c37653d6f6469dc3671968
van Hauser <vh@thc.org>
2020-06-22 07:16:24 +0200

    allow /tmp

--- src/afl-fuzz-run.c
+++ src/afl-fuzz-run.c
@@ -533,7 +533,7 @@ void sync_fuzzers(afl_state_t *afl) {
       s32         fd;
       struct stat st;
 
-      snprintf(path, sizeof (path), "%s/%s", qd_path, namelist[o]->d_name);
+      snprintf(path, sizeof(path), "%s/%s", qd_path, namelist[o]->d_name);
       afl->syncing_case = next_min_accept;
       next_min_accept++;
       o--;
