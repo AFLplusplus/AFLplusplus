e598fee60b3f95f67ee1726705babc1e02c3a0e9
van Hauser <vh@thc.org>
2020-07-26 15:55:03 +0200

    add last 60s exec/s stat

--- test/test-floatingpoint.c
+++ test/test-floatingpoint.c
@@ -8,16 +8,16 @@ __AFL_FUZZ_INIT();
 int main(void) {
 
   ssize_t bytes_read;
-  
+
   __AFL_INIT();
-  float *magic = (float*)__AFL_FUZZ_TESTCASE_BUF;
-  
+  float *magic = (float *)__AFL_FUZZ_TESTCASE_BUF;
+
   while (__AFL_LOOP(INT_MAX)) {
 
     if (__AFL_FUZZ_TESTCASE_LEN != sizeof(float)) return 1;
     /* 15 + 1/2 + 1/8 + 1/32 + 1/128 */
     if ((-*magic == 15.0 + 0.5 + 0.125 + 0.03125 + 0.0078125)) abort();
-  
+
   }
 
   return 0;
