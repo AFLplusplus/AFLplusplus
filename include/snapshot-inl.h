3fbeab325558a8d6046db4e85fb9722bb502014f
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-07-28 16:31:07 +0200

    lkm new api

--- include/snapshot-inl.h
+++ include/snapshot-inl.h
@@ -32,23 +32,26 @@
 
 #define AFL_SNAPSHOT_IOCTL_MAGIC 44313
 
-#define AFL_SNAPSHOT_EXCLUDE_VMRANGE _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 1, struct afl_snapshot_vmrange_args*)
-#define AFL_SNAPSHOT_INCLUDE_VMRANGE _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 2, struct afl_snapshot_vmrange_args*)
-#define AFL_SNAPSHOT_IOCTL_TAKE      _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 3, int)
-#define AFL_SNAPSHOT_IOCTL_RESTORE   _IO(AFL_SNAPSHOT_IOCTL_MAGIC, 4)
-#define AFL_SNAPSHOT_IOCTL_CLEAN     _IO(AFL_SNAPSHOT_IOCTL_MAGIC, 5)
+#define AFL_SNAPSHOT_IOCTL_DO _IO(AFL_SNAPSHOT_IOCTL_MAGIC, 1)
+#define AFL_SNAPSHOT_IOCTL_CLEAN _IO(AFL_SNAPSHOT_IOCTL_MAGIC, 2)
+#define AFL_SNAPSHOT_EXCLUDE_VMRANGE \
+  _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 3, struct afl_snapshot_vmrange_args *)
+#define AFL_SNAPSHOT_INCLUDE_VMRANGE \
+  _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 4, struct afl_snapshot_vmrange_args *)
+#define AFL_SNAPSHOT_IOCTL_TAKE _IOR(AFL_SNAPSHOT_IOCTL_MAGIC, 5, int)
+#define AFL_SNAPSHOT_IOCTL_RESTORE _IO(AFL_SNAPSHOT_IOCTL_MAGIC, 6)
 
 // Trace new mmaped ares and unmap them on restore.
-#define AFL_SNAPSHOT_MMAP  1
+#define AFL_SNAPSHOT_MMAP 1
 // Do not snapshot any page (by default all writeable not-shared pages
 // are shanpshotted.
 #define AFL_SNAPSHOT_BLOCK 2
 // Snapshot file descriptor state, close newly opened descriptors
-#define AFL_SNAPSHOT_FDS   4
+#define AFL_SNAPSHOT_FDS 4
 // Snapshot registers state
-#define AFL_SNAPSHOT_REGS  8
+#define AFL_SNAPSHOT_REGS 8
 // Perform a restore when exit_group is invoked
-#define AFL_SNAPSHOT_EXIT  16
+#define AFL_SNAPSHOT_EXIT 16
 // TODO(andrea) allow not COW snapshots (high perf on small processes)
 // Disable COW, restore all the snapshotted pages
 #define AFL_SNAPSHOT_NOCOW 32
@@ -70,16 +73,18 @@ static int afl_snapshot_init() {
 
 }
 
-static void afl_snapshot_exclude_vmrange(void* start, void* end) {
+static void afl_snapshot_exclude_vmrange(void *start, void *end) {
 
-  struct afl_snapshot_vmrange_args args = {(unsigned long)start, (unsigned long)end};
+  struct afl_snapshot_vmrange_args args = {(unsigned long)start,
+                                           (unsigned long)end};
   ioctl(afl_snapshot_dev_fd, AFL_SNAPSHOT_EXCLUDE_VMRANGE, &args);
 
 }
 
-static void afl_snapshot_include_vmrange(void* start, void* end) {
+static void afl_snapshot_include_vmrange(void *start, void *end) {
 
-  struct afl_snapshot_vmrange_args args = {(unsigned long)start, (unsigned long)end};
+  struct afl_snapshot_vmrange_args args = {(unsigned long)start,
+                                           (unsigned long)end};
   ioctl(afl_snapshot_dev_fd, AFL_SNAPSHOT_INCLUDE_VMRANGE, &args);
 
 }
@@ -87,6 +92,12 @@ static void afl_snapshot_include_vmrange(void* start, void* end) {
 static int afl_snapshot_take(int config) {
 
   return ioctl(afl_snapshot_dev_fd, AFL_SNAPSHOT_IOCTL_TAKE, config);
+
+}
+
+static int afl_snapshot_take(void) {
+
+  return ioctl(afl_snapshot_dev_fd, AFL_SNAPSHOT_IOCTL_DO);
 
 }
 
