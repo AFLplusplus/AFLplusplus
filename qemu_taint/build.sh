f98e2ef4bd173d34719f97f39da704453e7e855f
van Hauser <vh@thc.org>
2020-08-08 12:53:39 +0200

    cp inc

--- qemu_taint/build.sh
+++ qemu_taint/build.sh
@@ -2,5 +2,6 @@
 test -d qemu ||  git clone https://github.com/vanhauser-thc/qemu_taint qemu || exit 1
 cd qemu || exit 1
 test -d .git || { git stash ; git pull ; }
+cp -fv ../../include/config.h .
 ./build.sh
-cp -f ./afl-qemu-taint ../..
+cp -fv ./afl-qemu-taint ../..
