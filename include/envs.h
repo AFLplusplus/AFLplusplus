664daa2f3c7bb76253f6f82b827e1c33d0a0f462
van Hauser <vh@thc.org>
2020-09-01 01:12:40 +0200

    add qemu driver env var

--- include/envs.h
+++ include/envs.h
@@ -112,6 +112,7 @@ static char *afl_environment_variables[] = {
     "AFL_QEMU_COMPCOV_DEBUG",
     "AFL_QEMU_DEBUG_MAPS",
     "AFL_QEMU_DISABLE_CACHE",
+    "AFL_QEMU_DRIVER_NO_HOOK",
     "AFL_QEMU_PERSISTENT_ADDR",
     "AFL_QEMU_PERSISTENT_CNT",
     "AFL_QEMU_PERSISTENT_GPR",
