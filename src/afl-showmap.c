bea76b346ce278896d0b4a6f3b1d42e5c1c786c3
Dominik Maier <domenukk@gmail.com>
2020-08-31 18:29:45 +0200

    fixed build error

--- src/afl-showmap.c
+++ src/afl-showmap.c
@@ -636,9 +636,10 @@ static void usage(u8 *argv0) {
       "size\n"
       "              the target was compiled for\n"
       "AFL_PRELOAD: LD_PRELOAD / DYLD_INSERT_LIBRARIES settings for target\n"
-      "AFL_QUIET: do not print extra informational output\n",
+      "AFL_QUIET: do not print extra informational output\n"
       "AFL_FORKSRV_INIT_TMOUT: time the fuzzer waits for the target to come up, initially\n"
-      argv0, MEM_LIMIT, doc_path);
+
+      ,argv0, MEM_LIMIT, doc_path);
 
   exit(1);
 
