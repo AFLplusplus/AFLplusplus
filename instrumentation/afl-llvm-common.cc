e7851239acd168144cceed332a5a6689c012ab13
van Hauser <vh@thc.org>
2020-09-01 10:59:50 +0200

    update instrumentation modules

--- instrumentation/afl-llvm-common.cc
+++ instrumentation/afl-llvm-common.cc
@@ -344,9 +344,10 @@ static std::string getSourceName(llvm::Function *F) {
     (LLVM_VERSION_MAJOR == 3 && LLVM_VERSION_MINOR >= 7)
   if (Loc) {
 
+    StringRef   instFilename;
     DILocation *cDILoc = dyn_cast<DILocation>(Loc.getAsMDNode());
 
-    StringRef instFilename = cDILoc->getFilename();
+    if (cDILoc) { instFilename = cDILoc->getFilename(); }
 
     if (instFilename.str().empty()) {
 
