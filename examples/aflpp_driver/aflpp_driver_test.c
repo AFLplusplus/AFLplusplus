a32edb86404f2dafc9c6e5b172a41832c797897b
van Hauser <vh@thc.org>
2020-09-04 15:27:02 +0200

    fix driver test

--- examples/aflpp_driver/aflpp_driver_test.c
+++ examples/aflpp_driver/aflpp_driver_test.c
@@ -16,12 +16,12 @@ void __attribute__((noinline)) crashme(const uint8_t *Data, size_t Size) {
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
+  if (Size < 5) return 0;
+
   fprintf(stderr, "FUNC crc: %016llx len: %lu\n",
           hash64((u8 *)Data, (unsigned int)Size,
                  (unsigned long long int)0xa5b35705),
           Size);
-
-  if (Size < 5) return 0;
 
   crashme(Data, Size);
 
