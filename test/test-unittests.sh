11542de1a2973316dc346b7b8424e76e0c49e972
van Hauser <vh@thc.org>
2020-09-10 16:41:40 +0200

    fix unittests

--- test/test-unittests.sh
+++ test/test-unittests.sh
@@ -5,7 +5,7 @@
 $ECHO "$BLUE[*] Execution cmocka Unit-Tests $GREY"
 unset AFL_CC
 make -C .. unit || CODE=1 INCOMPLETE=1 :
+rm -rf unittests/unit_hash unittests/unit_rand
 
 . ./test-post.sh
 
-rm -rf unittests/unit_hash unittests/unit_rand
