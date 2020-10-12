b7e0490bcdaa7fa792a9dccfa5983e03af92730e
vanhauser-thc <vh@thc.org>
2020-10-12 03:44:34 +0200

    Revert "Merge branch 'memcache_marc' into dev"
    This reverts commit c03fbcedaa68db5324423975a34331287426f7c2, reversing
    changes made to dab017dddaaab6d836a590f7bba3eea3549758d2.

--- include/config.h
+++ include/config.h
@@ -295,15 +295,6 @@
 
 #define RESEED_RNG 100000
 
-/* The amount of entries in the testcase cache, held in memory.
-Decrease if RAM usage is high. */
-#define TESTCASE_CACHE_SIZE 3072
-
-#if TESTCASE_CACHE_SIZE < 4
-  #error \
-      "Dangerously low cache size: Set TESTCASE_CACHE_SIZE to 4 or more in config.h!"
-#endif
-
 /* Maximum line length passed from GCC to 'as' and used for parsing
    configuration files: */
 
