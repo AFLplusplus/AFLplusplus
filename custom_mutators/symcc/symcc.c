7a359597ec16d6cc84ce94e5a8fe9c5d2a67f4ba
hexcoder- <heiko@hexco.de>
2020-11-12 21:51:44 +0100

    another two mem leaks

--- custom_mutators/symcc/symcc.c
+++ custom_mutators/symcc/symcc.c
@@ -41,6 +41,7 @@ my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
 
   if ((data->mutator_buf = malloc(MAX_FILE)) == NULL) {
 
+    free(data);
     perror("mutator_buf alloc");
     return NULL;
 
