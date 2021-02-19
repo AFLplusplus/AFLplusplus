425cbf58c0390bc12e6014e871f3ef5809f6fba9
Julius Hohnerlein <julihoh@users.noreply.github.com>
2021-02-19 14:03:26 +0100

    add example for fallible custom mutator

--- custom_mutators/rust/example/src/lib.rs
+++ custom_mutators/rust/example/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 
-use custom_mutator::{export_mutator, CustomMutator};
+use custom_mutator::{export_mutator, fallible::FallibleCustomMutator, CustomMutator};
 use std::os::raw::c_uint;
 
 struct ExampleMutator;
@@ -40,6 +40,33 @@ impl CustomMutator for OwnBufferExampleMutator {
     ) -> Option<&'b [u8]> {
         self.own_buffer.reverse();
         Some(self.own_buffer.as_slice())
+    }
+}
+
+struct FallibleMutatorExample;
+
+impl FallibleCustomMutator for FallibleMutatorExample {
+    type Error = i32;
+
+    fn init(seed: c_uint) -> Result<Self, i32> {
+        Ok(Self)
+    }
+
+    fn fuzz<'b, 's: 'b>(
+        &'s mut self,
+        buffer: &'b mut [u8],
+        add_buff: Option<&[u8]>,
+        max_size: usize,
+    ) -> Result<Option<&'b [u8]>, i32> {
+        Err(1)
+    }
+
+    fn handle_err(err: i32) {
+        if std::env::var("DEBUG").is_ok() {
+            eprintln!("Error in custom mutator: {}", err);
+            // might panic in debug mode
+        }
+        // not panicing here -> continue execution
     }
 }
 
