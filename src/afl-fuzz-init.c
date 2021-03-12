d4fb7f8b4015297e1c74b28d671eba058cfb6366
realmadsci <71108352+realmadsci@users.noreply.github.com>
2021-03-12 15:53:42 -0500

    Add AFL_QEMU_CUSTOM_BIN environment flag
    In QEMU mode (-Q), setting AFL_QEMU_CUSTOM_BIN cause afl-fuzz to skip
    prepending afl-qemu-trace to your command line. Use this if you wish to use a
    custom afl-qemu-trace or if you need to modify the afl-qemu-trace arguments.

--- src/afl-fuzz-init.c
+++ src/afl-fuzz-init.c
@@ -2592,6 +2592,7 @@ void check_binary(afl_state_t *afl, u8 *fname) {
   }
 
   if (afl->afl_env.afl_skip_bin_check || afl->use_wine || afl->unicorn_mode ||
+      (afl->fsrv.qemu_mode && getenv("AFL_QEMU_CUSTOM_BIN")) ||
       afl->non_instrumented_mode) {
 
     return;
