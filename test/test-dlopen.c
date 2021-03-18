28b17790739da55edfd93830f013079be7c32174
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-03-18 21:34:12 +0100

    test-dlopen llvm test

---
+++ test/test-dlopen.c
@@ -0,0 +1,19 @@
+#include <dlfcn.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  if (!getenv("TEST_DLOPEN_TARGET"))
+    return 1;
+  void* lib = dlopen(getenv("TEST_DLOPEN_TARGET"), RTLD_LAZY);
+  if (!lib) {
+    perror(dlerror());
+    return 2;
+  }
+  int (*func)(int, char**) = dlsym(lib, "main_exported");
+  if (!func)
+    return 3;
+  
+  return func(argc, argv);
+
+}
