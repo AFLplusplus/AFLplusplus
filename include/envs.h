e73c7c59c14d0bff1c1b25aa264fe18b7cdcddaf
vanhauser-thc <vh@thc.org>
2021-03-19 14:55:25 +0100

    refactor finding binaries

--- include/envs.h
+++ include/envs.h
@@ -42,6 +42,7 @@ static char *afl_environment_variables[] = {
     "AFL_DEBUG_CHILD",
     "AFL_DEBUG_GDB",
     "AFL_DISABLE_TRIM",
+    "AFL_DISABLE_LLVM_INSTRUMENTATION",
     "AFL_DONT_OPTIMIZE",
     "AFL_DRIVER_STDERR_DUPLICATE_FILENAME",
     "AFL_DUMB_FORKSRV",
