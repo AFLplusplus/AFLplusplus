84a99f49b84b5380b9b2d79354bd9892216b180d
vanhauser-thc <vh@thc.org>
2021-03-29 19:07:47 +0200

    remove unneeded var

--- instrumentation/afl-compiler-rt.o.c
+++ instrumentation/afl-compiler-rt.o.c
@@ -99,12 +99,10 @@ int __afl_selective_coverage_temp = 1;
 PREV_LOC_T __afl_prev_loc[NGRAM_SIZE_MAX];
 PREV_LOC_T __afl_prev_caller[CTX_MAX_K];
 u32        __afl_prev_ctx;
-u32        __afl_cmp_counter;
 #else
 __thread PREV_LOC_T __afl_prev_loc[NGRAM_SIZE_MAX];
 __thread PREV_LOC_T __afl_prev_caller[CTX_MAX_K];
 __thread u32        __afl_prev_ctx;
-__thread u32        __afl_cmp_counter;
 #endif
 
 int __afl_sharedmem_fuzzing __attribute__((weak));
