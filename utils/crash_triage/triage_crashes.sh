a4cb2414d5a26699f999667752d3461da20d3f82
hexcoder- <heiko@hexco.de>
2021-06-09 21:29:41 +0200

    Revert "add test cases for splitting integer comparisons"
    This reverts commit e0aa411647e1a525a3a0488d929ec71611388d54.

--- utils/crash_triage/triage_crashes.sh
+++ utils/crash_triage/triage_crashes.sh
@@ -65,11 +65,7 @@ if [ ! -f "$BIN" -o ! -x "$BIN" ]; then
 fi
 
 if [ ! -d "$DIR/queue" ]; then
-<<<<<<< Updated upstream
   echo "[-] Error: directory '$DIR' not found or not created by afl-fuzz." 1>&2
-=======
-  echo "[-] Error: directory '$DIR/queue' not found or not created by afl-fuzz." 1>&2
->>>>>>> Stashed changes
   exit 1
 fi
 
