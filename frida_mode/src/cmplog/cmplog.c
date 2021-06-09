e81660d0409162271069e1fe8c9880675d79516d
Your Name <you@example.com>
2021-06-09 18:43:21 +0100

    Fix complog issue with changing address space

--- frida_mode/src/cmplog/cmplog.c
+++ frida_mode/src/cmplog/cmplog.c
@@ -1,3 +1,8 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <syscall.h>
+
 #include "frida-gum.h"
 
 #include "debug.h"
@@ -9,6 +14,7 @@
 extern struct cmp_map *__afl_cmp_map;
 
 static GArray *cmplog_ranges = NULL;
+static int     fd_null = -1;
 
 static gboolean cmplog_range(const GumRangeDetails *details,
                              gpointer               user_data) {
@@ -24,6 +30,39 @@ static gint cmplog_sort(gconstpointer a, gconstpointer b) {
 
   return ((GumMemoryRange *)b)->base_address -
          ((GumMemoryRange *)a)->base_address;
+
+}
+
+static int cmplog_create_temp(void) {
+
+  const char *tmpdir = g_get_tmp_dir();
+  OKF("CMPLOG Temporary directory: %s", tmpdir);
+  gchar *fname = g_strdup_printf("%s/frida-cmplog-XXXXXX", tmpdir);
+  OKF("CMPLOG Temporary file template: %s", fname);
+  int fd = mkstemp(fname);
+  OKF("CMPLOG Temporary file: %s", fname);
+
+  if (fd < 0) {
+
+    FATAL("Failed to create temp file: %s, errno: %d", fname, errno);
+
+  }
+
+  if (unlink(fname) < 0) {
+
+    FATAL("Failed to unlink temp file: %s (%d), errno: %d", fname, fd, errno);
+
+  }
+
+  if (ftruncate(fd, 0) < 0) {
+
+    FATAL("Failed to unlink temp file: %s (%d), errno: %d", fname, fd, errno);
+
+  }
+
+  g_free(fname);
+
+  return fd;
 
 }
 
@@ -43,6 +82,13 @@ void cmplog_init(void) {
         range->base_address, range->base_address + range->size);
 
   }
+
+  /*
+   * We can't use /dev/null or /dev/zero for this since it appears that they
+   * don't validate the input buffer. Persumably as an optimization because they
+   * don't actually write any data. The file will be deleted on close.
+   */
+  fd_null = cmplog_create_temp();
 
 }
 
@@ -67,6 +113,9 @@ gboolean cmplog_is_readable(guint64 addr, size_t size) {
    */
   if (addr < DEFAULT_MMAP_MIN_ADDR) { return false; }
 
+  /* Check our addres/length don't wrap around */
+  if (SIZE_MAX - addr < size) { return false; }
+
   GumAddress inner_base = addr;
   GumAddress inner_limit = inner_base + size;
 
@@ -80,6 +129,27 @@ gboolean cmplog_is_readable(guint64 addr, size_t size) {
       return true;
 
   }
+
+  /*
+   * Our address map can change (e.g. stack growth), use write as a fallback to
+   * validate our address.
+   */
+  ssize_t written = syscall(__NR_write, fd_null, (void *)addr, size);
+
+  /*
+   * Seek back to the start, we don't want our temp file to continue growing!
+   */
+  if (ftruncate(fd_null, 0) < 0) {
+
+    FATAL("Failed to truncate fd_null (%d), errno: %d", fd_null, errno);
+
+  }
+
+  /*
+   * If the write succeeds, then the buffer must be valid otherwise it would
+   * return EFAULT
+   */
+  if (written > 0 && (size_t)written == size) { return true; }
 
   return false;
 
