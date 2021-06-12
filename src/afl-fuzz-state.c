0d78c101505183d5458bfc35bb1cd38ab1da277d
vanhauser-thc <vh@thc.org>
2021-06-12 10:35:22 +0200

    fix merge

--- src/afl-fuzz-state.c
+++ src/afl-fuzz-state.c
@@ -205,6 +205,8 @@ void read_afl_environment(afl_state_t *afl, char **envp) {
                               afl_environment_variable_len)) {
 
             afl->afl_env.afl_try_affinity =
+                get_afl_env(afl_environment_variables[i]) ? 1 : 0;
+
           } else if (!strncmp(env, "AFL_NO_INTERESTING",
 
                               afl_environment_variable_len)) {
