c6b77d2d05b07040c6599d8c9a142f0ad96ced62
vanhauser-thc <vh@thc.org>
2021-06-23 10:53:00 +0200

    force disable llvm instrumentation for frida

--- src/afl-fuzz-cmplog.c
+++ src/afl-fuzz-cmplog.c
@@ -33,7 +33,7 @@ void cmplog_exec_child(afl_forkserver_t *fsrv, char **argv) {
 
   setenv("___AFL_EINS_ZWEI_POLIZEI___", "1", 1);
 
-  if (fsrv->qemu_mode) { setenv("AFL_DISABLE_LLVM_INSTRUMENTATION", "1", 0); }
+  if (fsrv->qemu_mode || fsrv->frida_mode) { setenv("AFL_DISABLE_LLVM_INSTRUMENTATION", "1", 0); }
 
   if (!fsrv->qemu_mode && !fsrv->frida_mode && argv[0] != fsrv->cmplog_binary) {
 
