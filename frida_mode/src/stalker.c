2d8050c3976eb0f7fbc3d6b83d824a1b33b0b581
Dominik Maier <domenukk@gmail.com>
2021-07-15 11:52:04 +0200

    deleted duplicate line after merge

--- frida_mode/src/stalker.c
+++ frida_mode/src/stalker.c
@@ -43,9 +43,6 @@ void stalker_init(void) {
   /* *NEVER* stalk the stalker, only bad things will ever come of this! */
   gum_process_enumerate_ranges(GUM_PAGE_EXECUTE, stalker_exclude_self, NULL);
 
-  /* *NEVER* stalk the stalker, only bad things will ever come of this! */
-  gum_process_enumerate_ranges(GUM_PAGE_EXECUTE, stalker_exclude_self, NULL);
-
 }
 
 GumStalker *stalker_get(void) {
