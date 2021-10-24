a7ee11a1747347847b06a4226f2800dd780f7c16
vanhauser-thc <vh@thc.org>
2021-10-24 19:35:58 +0200

    fix

--- src/afl-forkserver.c
+++ src/afl-forkserver.c
@@ -603,7 +603,7 @@ void afl_fsrv_start(afl_forkserver_t *fsrv, char **argv,
   /* Wait for the fork server to come up, but don't wait too long. */
 
   rlen = 0;
-  if (fsrv->exec_tmout) {
+  if (fsrv->init_tmout) {
 
     u32 time_ms = read_s32_timed(fsrv->fsrv_st_fd, &status, fsrv->init_tmout,
                                  stop_soon_p);
@@ -614,7 +614,7 @@ void afl_fsrv_start(afl_forkserver_t *fsrv, char **argv,
       if (tmp_pid > 0) {
 
         kill(tmp_pid, fsrv->kill_signal);
-        fsrv->fsrv_pid = 1;
+        fsrv->fsrv_pid = -1;
 
       }
 
@@ -625,7 +625,7 @@ void afl_fsrv_start(afl_forkserver_t *fsrv, char **argv,
       if (tmp_pid > 0) {
 
         kill(tmp_pid, fsrv->kill_signal);
-        fsrv->fsrv_pid = 1;
+        fsrv->fsrv_pid = -1;
 
       }
 
@@ -1301,7 +1301,7 @@ fsrv_run_result_t afl_fsrv_run_target(afl_forkserver_t *fsrv, u32 timeout,
 
   }
 
-  if (!WIFSTOPPED(fsrv->child_status)) { fsrv->child_pid = 0; }
+  if (!WIFSTOPPED(fsrv->child_status)) { fsrv->child_pid = -1 ; }
 
   fsrv->total_execs++;
 
