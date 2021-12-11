10afd52b219ec8b53b592331ff1708f434ab6ea7
Rick van Schijndel <rol3517@gmail.com>
2021-12-11 15:48:58 +0100

    afl-cc: fix incorrect CLANGPP_BIN
    This caused CMake to complain that the C compiler was set to C++ mode,
    causing the CMake configuration step to fail for all targets.
    
    aflplusplus was built with
    
    make source-only -j8 LLVM_BINDIR= AFL_REAL_LD=

--- src/afl-cc.c
+++ src/afl-cc.c
@@ -395,7 +395,7 @@ static void edit_params(u32 argc, char **argv, char **envp) {
           snprintf(llvm_fullpath, sizeof(llvm_fullpath), "%s/clang",
                    LLVM_BINDIR);
         else
-          snprintf(llvm_fullpath, sizeof(llvm_fullpath), CLANGPP_BIN);
+          snprintf(llvm_fullpath, sizeof(llvm_fullpath), CLANG_BIN);
         alt_cc = llvm_fullpath;
 
       }
