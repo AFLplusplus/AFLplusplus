be7333f050caa87ab90db2ee69c2bd23c4e71c31
yuawn <ssspeed00@gmail.com>
2021-12-11 10:20:40 +0000

    code format

--- test/test-fp_Infcases.c
+++ test/test-fp_Infcases.c
@@ -6,23 +6,23 @@
 
 #include <assert.h>
 #define _GNU_SOURCE
-#include <math.h> /* for NaNs and infinity values */
+#include <math.h>                           /* for NaNs and infinity values */
 
 int main() {
 
   volatile FLOAT_TYPE a, b;
 
 #ifdef INFINITY
-  FLOAT_TYPE inf = (FLOAT_TYPE) INFINITY;
+  FLOAT_TYPE inf = (FLOAT_TYPE)INFINITY;
 #else
-  FLOAT_TYPE inf = 1.0 / 0.0; /* produces infinity */
+  FLOAT_TYPE inf = 1.0 / 0.0;                          /* produces infinity */
 #endif
   FLOAT_TYPE negZero = 1.0 / -inf;
   FLOAT_TYPE posZero = 0.0;
 
   /* plus infinity */
-  a = (1.0 / 0.0);	/* positive infinity */
-  b = (1.0 / 0.0);	/* positive infinity */
+  a = (1.0 / 0.0);                                     /* positive infinity */
+  b = (1.0 / 0.0);                                     /* positive infinity */
   assert(!(a < b));
   assert((a <= b));
   assert(!(a > b));
@@ -30,7 +30,7 @@ int main() {
   assert(!(a != b));
   assert((a == b));
 
-  b = -(1.0 / 0.0);	/* negative infinity */
+  b = -(1.0 / 0.0);                                    /* negative infinity */
   assert(!(a < b));
   assert(!(a <= b));
   assert((a > b));
@@ -38,7 +38,7 @@ int main() {
   assert((a != b));
   assert(!(a == b));
 
-  b = 1.0 / -(1.0 / 0.0);	/* negative 0 */
+  b = 1.0 / -(1.0 / 0.0);                                     /* negative 0 */
   assert(!(a < b));
   assert(!(a <= b));
   assert((a > b));
@@ -46,7 +46,7 @@ int main() {
   assert((a != b));
   assert(!(a == b));
 
-  b = 0.0;	/* positive 0 */
+  b = 0.0;                                                    /* positive 0 */
   assert(!(a < b));
   assert(!(a <= b));
   assert((a > b));
@@ -71,8 +71,8 @@ int main() {
   assert(!(a == b));
 
   /* negative infinity */
-  a = -(1.0 / 0.0);	
-  b = (1.0 / 0.0);	/* positive infinity */
+  a = -(1.0 / 0.0);
+  b = (1.0 / 0.0);                                     /* positive infinity */
   assert((a < b));
   assert((a <= b));
   assert(!(a > b));
@@ -80,7 +80,7 @@ int main() {
   assert((a != b));
   assert(!(a == b));
 
-  b = -(1.0 / 0.0);	/* negative infinity */
+  b = -(1.0 / 0.0);                                    /* negative infinity */
   assert(!(a < b));
   assert((a <= b));
   assert(!(a > b));
@@ -88,7 +88,7 @@ int main() {
   assert(!(a != b));
   assert((a == b));
 
-  b = 1.0 / -(1.0 / 0.0);	/* negative 0 */
+  b = 1.0 / -(1.0 / 0.0);                                     /* negative 0 */
   assert((a < b));
   assert((a <= b));
   assert(!(a > b));
@@ -96,7 +96,7 @@ int main() {
   assert((a != b));
   assert(!(a == b));
 
-  b = 0.0;	/* positive 0 */
+  b = 0.0;                                                    /* positive 0 */
   assert((a < b));
   assert((a <= b));
   assert(!(a > b));
--- test/test-fp_minusZerocases.c
+++ test/test-fp_minusZerocases.c
@@ -6,15 +6,15 @@
 
 #include <assert.h>
 #define _GNU_SOURCE
-#include <math.h> /* for NaNs and infinity values */
+#include <math.h>                           /* for NaNs and infinity values */
 
 int main() {
 
   volatile FLOAT_TYPE a, b;
 
   /* negative zero */
-  a = 1.0 / -(1.0 / 0.0);	/* negative 0 */
-  b = 0.0;	/* positive 0 */
+  a = 1.0 / -(1.0 / 0.0);                                     /* negative 0 */
+  b = 0.0;                                                    /* positive 0 */
   assert(!(a < b));
   assert((a <= b));
   assert(!(a > b));
@@ -22,8 +22,8 @@ int main() {
   assert(!(a != b));
   assert((a == b));
 
-  a = 1.0 / -(1.0 / 0.0);	/* negative 0 */
-  b = 1.0 / -(1.0 / 0.0);	/* negative 0 */
+  a = 1.0 / -(1.0 / 0.0);                                     /* negative 0 */
+  b = 1.0 / -(1.0 / 0.0);                                     /* negative 0 */
   assert(!(a < b));
   assert((a <= b));
   assert(!(a > b));
