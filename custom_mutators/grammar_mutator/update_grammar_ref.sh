122ece67cf330bff939b1096a6802bfcdf89312f
CityOfLight77 <75525669+CityOfLight77@users.noreply.github.com>
2021-12-21 05:16:03 +0700

    Update grammar_ref.sh
    Add option to checkout from dev branch, and use stable branch as default if no branch provided

--- custom_mutators/grammar_mutator/update_grammar_ref.sh
+++ custom_mutators/grammar_mutator/update_grammar_ref.sh
@@ -2,25 +2,33 @@
 
 ##################################################
 # AFL++ tool to update a git ref.
-# Usage: ./<script>.sh <new commit hash>
+# Usage: ./<script>.sh <new commit hash> <branch>
 # If no commit hash was provided, it'll take HEAD.
+# If no branch was provided, it'll take stable branch.
 ##################################################
 
 TOOL="grammar mutator"
 VERSION_FILE='./GRAMMAR_VERSION'
 REPO_FOLDER='./grammar_mutator'
 THIS_SCRIPT=`basename $0`
-BRANCH="stable"
 
 NEW_VERSION="$1"
+BRANCH="$2"
 
 if [ "$NEW_VERSION" = "-h" ]; then
   echo "Internal script to update bound $TOOL version."
   echo
-  echo "Usage: $THIS_SCRIPT <new commit hash>"
+  echo "Usage: $THIS_SCRIPT <new commit hash> <branch>"
   echo "If no commit hash is provided, will use HEAD."
+  echo "If no branch was provided, it'll take stable branch."
   echo "-h to show this help screen."
   exit 1
+fi
+
+if [ "$BRANCH" = "dev" ]; then
+  BRANCH="dev"
+elif [ -z "$BRANCH" ]; then
+  BRANCH="stable"
 fi
 
 git submodule init && git submodule update ./grammar_mutator || exit 1
@@ -44,7 +52,8 @@ git checkout "$NEW_VERSION" || exit 1
 
 cd ..
 
-rm "$VERSION_FILE"
+rm "$
+"
 echo "$NEW_VERSION" > "$VERSION_FILE"
 
 echo "Done. New $TOOL version is $NEW_VERSION."
