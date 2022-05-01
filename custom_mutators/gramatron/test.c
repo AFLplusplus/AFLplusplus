bfd83e31ef9a326ea9fe53dd50259c4c991637e1
Helen Yi <yihellen@outlook.com>
2022-05-01 06:18:12 +0000

    add build hashmap; WIP; test if constructed correctly tomorrow

--- custom_mutators/gramatron/test.c
+++ custom_mutators/gramatron/test.c
@@ -6,6 +6,7 @@
 #define NUMINPUTS 50
 #define MAX_PROGRAM_LENGTH 2000
 #define MAX_WALK_LENGTH 1000 // maximum length of terminals in a walk
+#define MAX_TERMINAL_STATES 50
 
 state *create_pda(u8 *automaton_file) {
 
@@ -122,56 +123,139 @@ void find_automaton_walk(char *state_curr, state *pda) {
 
 }
 
-typedef struct {
+struct terminal_meta {
 
-  size_t t;
+  int state_name;
+  size_t trigger_idx;
+  int dest;
 
-} test;
+} ;
 
-// TODO: generate a map
-// map a symbol to (state, trigger_idx)
-void create_pda_hashmap() {
-  // tryout hashmap
+struct terminal_arr {
+
+  struct terminal_meta* start;
+  size_t len;
+
+} ;
+
+// TODO: delete
+void output_hashmap(any_t placeholder, any_t item) {
+  struct terminal_meta* tmp = item;
+  printf("%d\n", tmp->state_name);
+}
+
+int free_terminal_arr(any_t placeholder, any_t item) {
+  struct terminal_arr* tmp = item;
+  free(tmp->start);
+  free(tmp);
+  return MAP_OK;
+}
+
+void free_hashmap(map_t m) {
+  int r = hashmap_iterate(m, &free_terminal_arr, NULL);
+  if (!r) printf("free hashmap items successfully!\n");
+  else printf("free hashmap items failed");
+  hashmap_free(m);
+}
+
+// map a symbol to a list of (state, trigger_idx)
+map_t create_pda_hashmap(state* pda) {
+  int state_idx, trigger_idx, r; // r is the return result for hashmap operation 
   map_t m = hashmap_new();
-  char k[100] = "key";
-  test* vptr = (test *)malloc(sizeof(test));
-  vptr->t = 9;
-  any_t v = vptr;
-  any_t grptr;
-  int r = hashmap_put(m, k, v);
-  if (!r) {
-    printf("add elements to hashmap succeed\n");
-    int gr = hashmap_get(m, k, vptr);
-    printf("%d\n", gr);
+  // iterate over pda
+  for (state_idx = 0; state_idx < numstates; state_idx++) {
+    printf("------ The state idx is %d ------\n", state_idx);
+    if (state_idx == final_state) continue;
+    state* state_curr = pda + state_idx;
+    for (trigger_idx = 0; trigger_idx < state_curr->trigger_len; trigger_idx++) {
+      printf("------ The trigger idx is %d ------\n", trigger_idx);
+      trigger* trigger_curr = state_curr->ptr + trigger_idx;
+      char* symbol_curr = trigger_curr->term;
+      struct terminal_arr* terminal_arr_curr;
+      r = hashmap_get(m, symbol_curr, (any_t*)&terminal_arr_curr);
+      if (r) {
+        // the symbol is not in the map
+        printf("Symbol %s is not in map\n", symbol_curr);
+        struct terminal_arr* new_terminal_arr = (struct terminal_arr*)malloc(sizeof(struct terminal_arr));
+        new_terminal_arr->start = (struct terminal_meta*)calloc(numstates, sizeof(struct terminal_meta));
+        printf("allocate new memory address %p\n", new_terminal_arr->start);
+        new_terminal_arr->start->state_name = state_idx;
+        new_terminal_arr->start->dest = trigger_curr->dest;
+        new_terminal_arr->start->trigger_idx = trigger_idx;
+        new_terminal_arr->len = 1;
+        printf("Symbol %s is included in %zu edges\n", symbol_curr, new_terminal_arr->len);
+        r = hashmap_put(m, symbol_curr, new_terminal_arr);
+        if (r) {
+          printf("hashmap put failed\n");
+        }
+        else {
+          printf("hashmap put succeeded\n");
+        }
+      }
+      else {
+        // the symbol is already in map
+        // append to terminal array
+        // no need to touch start
+        printf("Symbol %s is in map\n", symbol_curr);
+        struct terminal_meta* modify = terminal_arr_curr->start + terminal_arr_curr->len;
+        modify->state_name = state_idx;
+        modify->trigger_idx = trigger_idx;
+        modify->dest = trigger_curr->dest;
+        terminal_arr_curr->len++;
+        printf("Symbol %s is included in %zu edges\n", symbol_curr, terminal_arr_curr->len);
+      }
+
+    }
   }
-  else {printf("fail\n");}
-  free(vptr);
+  return m;
+
+  // tryout hashmap
+  // map_t m = hashmap_new();
+  // char k[100] = "key";
+  // struct terminal_meta* vptr = (struct terminal_meta *)malloc(sizeof(struct terminal_meta));
+  // vptr->state_name = 9;
+  // vptr->trigger_idx = 0;
+  // any_t v = vptr;
+  // struct terminal_meta* grptr;
+  // int r = hashmap_put(m, k, v);
+  // if (!r) {
+  //   printf("add elements to hashmap succeed\n");
+  //   int gr = hashmap_get(m, k, &grptr);
+  //   // printf("%d\n", gr);
+  //   hashmap_iterate(m, &output_hashmap, NULL);
+  // }
+  // else {printf("fail\n");}
+  // free(vptr);
+
+  // TODO: deallocate all the terminal arrays (not here!)
+  // TODO: deallocate the hashmap (not here)
 }
 
 
 int main(int argc, char *argv[]) {
   char automaton_path[] = "/root/gramatron-artifact/grammars/gt_bugs/mruby-1/source_automata.json";
-  char * pda = create_pda(automaton_path);
-  char program_path[] = "/root/gramatron-artifact/fuzzers/AFLplusplus/custom_mutators/gramatron/example";
-  FILE* ptr;
-  ptr = fopen(program_path, "r");
-  if (NULL == ptr) {
-    printf("file can't be opened \n");
-  }
-  char ch;
-  char program[MAX_PROGRAM_LENGTH];
-  int i = 0;
-  do {
-    ch = fgetc(ptr);
-    program[i] = ch;
-    printf("%c", ch);
-    i ++;
-  } while (ch != EOF);
-  Array * res;
-  res = (Array *)calloc(1, sizeof(Array));
-  initArray(res, INIT_SIZE);
-  create_pda_hashmap();
-  free(res);
+  state * pda = create_pda((u8 *)automaton_path);
+  // char program_path[] = "/root/gramatron-artifact/fuzzers/AFLplusplus/custom_mutators/gramatron/example";
+  // FILE* ptr;
+  // ptr = fopen(program_path, "r");
+  // if (NULL == ptr) {
+  //   printf("file can't be opened \n");
+  // }
+  // char ch;
+  // char program[MAX_PROGRAM_LENGTH];
+  // int i = 0;
+  // do {
+  //   ch = fgetc(ptr);
+  //   program[i] = ch;
+  //   printf("%c", ch);
+  //   i ++;
+  // } while (ch != EOF);
+  // Array * res;
+  // res = (Array *)calloc(1, sizeof(Array));
+  // initArray(res, INIT_SIZE);
+  map_t m = create_pda_hashmap((struct state*)pda);
+  free_hashmap(m);
+  // free(res);
   free(pda);
   return 0;
   // char *         mode;
