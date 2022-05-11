310ed26ef0239020d8e194a46f7dd00d0741e983
Helen Yi <yihellen@outlook.com>
2022-05-11 00:51:19 +0000

    commit test makefile

--- custom_mutators/gramatron/testMakefile.mk
+++ custom_mutators/gramatron/testMakefile.mk
@@ -1,2 +1,5 @@
 test: test.c
-	gcc -O3 -g -fPIC -Wno-unused-result -Wl,--allow-multiple-definition -I../../include -o test -I. -I/prg/dev/include test.c gramfuzz-helpers.c gramfuzz-mutators.c gramfuzz-util.c hashmap.c ../../src/afl-performance.o json-c/.libs/libjson-c.a
+	gcc -g -fPIC -Wno-unused-result -Wl,--allow-multiple-definition -I../../include -o test -I. -I/prg/dev/include test.c gramfuzz-helpers.c gramfuzz-mutators.c gramfuzz-util.c hashmap.c ../../src/afl-performance.o json-c/.libs/libjson-c.a
+
+clean:
+	rm -rf test
