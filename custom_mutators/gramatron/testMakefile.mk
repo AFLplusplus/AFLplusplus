c9213c8975851974069d6d2292ecad94c15d39af
Helen Yi <yihellen@outlook.com>
2022-05-25 01:32:11 +0000

    add makefile to test.c for testing

--- custom_mutators/gramatron/testMakefile.mk
+++ custom_mutators/gramatron/testMakefile.mk
@@ -1,21 +1,2 @@
-CC=gcc
-
-OBJ = test.o gramfuzz-helpers.o gramfuzz-mutators.o gramfuzz-util.o hashmap.o ../../src/afl-performance.o json-c/.libs/libjson-c.a
-
-DEPS = afl-fuzz.h gramfuzz.h 
-
-CFLAGS=-g -fPIC -Wno-unused-result -Wl,--allow-multiple-definition -I. -I../../include -I/prg/dev/include
-
-%.o: %.c $(DEPS)
-	$(CC) -c -o $@ $< $(CFLAGS)
-
-
-test: $(OBJ)
-	$(CC) -o $@ $^ $(CFLAGS)
-
-
-# test: test.c
-# 	gcc -g -fPIC -Wno-unused-result -Wl,--allow-multiple-definition -I../../include -o test -I. -I/prg/dev/include test.c gramfuzz-helpers.c gramfuzz-mutators.c gramfuzz-util.c hashmap.c ../../src/afl-performance.o json-c/.libs/libjson-c.a
-
-clean:
-	rm -rf test
+test: test.c
+	gcc -g -fPIC -Wno-unused-result -Wl,--allow-multiple-definition -I../../include -o test -I. -I/prg/dev/include test.c gramfuzz-helpers.c gramfuzz-mutators.c gramfuzz-util.c hashmap.c ../../src/afl-performance.o json-c/.libs/libjson-c.a
