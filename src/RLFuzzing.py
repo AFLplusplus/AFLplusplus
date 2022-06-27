e94b7d73325c3af34312911d6ad35ccabda9b037
sjmluo <simonjluo@gmail.com>
2022-06-27 16:14:08 +1000

    testing inital message with sched->exec_map

---
+++ src/RLFuzzing.py
@@ -0,0 +1,14 @@
+import sysv_ipc
+import numpy as np
+import struct
+
+BUFF_SIZE = 64
+
+try:
+	mq = sysv_ipc.MessageQueue(1234, sysv_ipc.IPC_CREAT)
+
+	while True:
+		message, mtype = mq.receive()
+		if mtype == 1:
+			sched_exec_map = struct.unpack("sched->exec_map", message)
+			print(f"Scheduler exec_map: {sched_exec_map}")
