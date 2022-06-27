c3f19f00d85b55225f30425fe59fdda3d6c0fcd1
sjmluo <simonjluo@gmail.com>
2022-06-27 22:06:13 +1000

    testing toy example for IPC

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -8,11 +8,10 @@ try:
     mq = sysv_ipc.MessageQueue(1234, sysv_ipc.IPC_CREAT)
 
     while True:
-        print('here')
         message, mtype = mq.receive()
-        print(mtype)
         if mtype == 1:
-            afl_fsrv_map_size = struct.unpack("afl->fsrv.map_size", message)
+            # afl_fsrv_map_size = struct.unpack("afl->fsrv.map_size", message)
+            afl_fsrv_map_size = message.decode()
             print(f"afl->fsrv.map_size: {afl_fsrv_map_size}")
 
 except sysv_ipc.ExistentialError:
--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -2264,10 +2264,11 @@ int main(int argc, char **argv_orig, char **envp) {
     if (true) {
       data.data_type = 1;
       // memcpy(data.data_buff, &afl->fsrv.map_size, sizeof(u32));
-      double msg_double1 = 1234.56789;
-      double msg_double2 = 9876.12345;
-      memcpy(data.data_buff, &msg_double1, sizeof(double));
-      memcpy(data.data_buff+sizeof(double), &msg_double2, sizeof(double));
+      // double msg_double1 = 1234.56789;
+      // double msg_double2 = 9876.12345;
+      // memcpy(data.data_buff, &msg_double1, sizeof(double));
+      // memcpy(data.data_buff+sizeof(double), &msg_double2, sizeof(double));
+      sprintf( data.data_buff, "string from C");
       if (-1 == msgsnd(msqid, &data, sizeof( t_data) - sizeof( long), 0)) {
         perror( "msgsnd() failed");
         exit(1);
