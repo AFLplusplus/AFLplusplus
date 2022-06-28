a558bf5cc89935dd04fb282bc5c8b1986be37631
sjmluo <simonjluo@gmail.com>
2022-06-28 23:25:49 +1000

    added different data types to IPC

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -24,7 +24,7 @@ class RLFuzzing:
                 # afl_fsrv_map_size = message.decode()
                 print(f"afl->fsrv.map_size: {afl_fsrv_map_size}")
                 print(f"mtype: {mtype}")
-                self.send_messenges(mtype)
+                # self.send_messenges(mtype)
 
         except sysv_ipc.ExistentialError:
             print("ERROR: message queue creation failed")
@@ -32,8 +32,8 @@ class RLFuzzing:
     def send_messenges(self, mtype, BUFF_SIZE_SENDER=64):
         if mtype == 1:
             msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.double).reshape((2,BUFF_SIZE_SENDER//2))
-        elif mtype == 2:
-            msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.uint8).reshape((2,BUFF_SIZE_SENDER//2))
+        # elif mtype == 2:
+        #     msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.uint8).reshape((2,BUFF_SIZE_SENDER//2))
 
         try:
             mq_sender = sysv_ipc.MessageQueue(2, sysv_ipc.IPC_CREAT)
