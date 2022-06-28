da0a73335e0c65e5cc23523c0b7ef08d1148aa51
sjmluo <simonjluo@gmail.com>
2022-06-28 23:00:59 +1000

    added different data types to IPC

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -32,8 +32,10 @@ class RLFuzzing:
     def send_messenges(self, mtype, BUFF_SIZE_SENDER=64):
         if mtype == 1:
             msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.double).reshape((2,BUFF_SIZE_SENDER//2))
+            msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.uint8).reshape((2,BUFF_SIZE_SENDER//2))
+
         elif mtype == 2:
-            msg_npy = np.arange(BUFF_SIZE_SENDER//2, dtype=np.uint8).reshape((2,BUFF_SIZE_SENDER//2))
+            msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.uint8).reshape((2,BUFF_SIZE_SENDER//2))
 
         try:
             mq_sender = sysv_ipc.MessageQueue(2, sysv_ipc.IPC_CREAT)
