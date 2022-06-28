2802a1f0871d11c6fd9d4a7eb7d115edd8c5569d
sjmluo <simonjluo@gmail.com>
2022-06-28 14:13:48 +1000

    enclosed functions into a class

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -2,26 +2,39 @@ import sysv_ipc
 import numpy as np
 import struct
 
-BUFF_SIZE_RECIEVER = 32
-BUFF_SIZE_SENDER = 64
-while True:
-    try:
-        mq_reciever = sysv_ipc.MessageQueue(1, sysv_ipc.IPC_CREAT)
 
-        message, mtype = mq_reciever.receive()
-        afl_fsrv_map_size = np.frombuffer(message, dtype=np.double)
-        # afl_fsrv_map_size = message.decode()
-        print(f"afl->fsrv.map_size: {afl_fsrv_map_size}")
+class RLFuzzing:
+    def __init__(self):
+        return
 
-    except sysv_ipc.ExistentialError:
-        print("ERROR: message queue creation failed")
+    def recieve_messages(self, BUFF_SIZE_RECIEVER = 32):
+        try:
+            mq_reciever = sysv_ipc.MessageQueue(1, sysv_ipc.IPC_CREAT)
 
-    msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.double).reshape((2,BUFF_SIZE_SENDER//2))
+            message, mtype = mq_reciever.receive()
+            afl_fsrv_map_size = np.frombuffer(message, dtype=np.double)
+            # afl_fsrv_map_size = message.decode()
+            print(f"afl->fsrv.map_size: {afl_fsrv_map_size}")
 
-    try:
-        mq_sender = sysv_ipc.MessageQueue(2, sysv_ipc.IPC_CREAT)
-        TYPE = 1
-        mq_sender.send(msg_npy.tobytes(order='C'), True, type=TYPE)
+        except sysv_ipc.ExistentialError:
+            print("ERROR: message queue creation failed")
 
-    except sysv_ipc.ExistentialError:
-        print("ERROR: message queue creation failed")
+    def send_messenges(self, BUFF_SIZE_SENDER = 64):
+        msg_npy = np.arange(BUFF_SIZE_SENDER, dtype=np.double).reshape((2,BUFF_SIZE_SENDER//2))
+
+        try:
+            mq_sender = sysv_ipc.MessageQueue(2, sysv_ipc.IPC_CREAT)
+            TYPE = 1
+            mq_sender.send(msg_npy.tobytes(order='C'), True, type=TYPE)
+
+        except sysv_ipc.ExistentialError:
+            print("ERROR: message queue creation failed")
+
+
+
+if __name__ == "__main__":
+    RLF = RLFuzzing()
+    while True:
+        RLF.recieve_messages()
+        RLF.send_messenges()
+
