994c68985d6285ae3be75a176dfc079eced70115
sjmluo <simonjluo@gmail.com>
2022-06-28 23:04:24 +1000

    added different data types to IPC

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -2290,7 +2290,7 @@ int main(int argc, char **argv_orig, char **envp) {
       t_recieve_double_data recieve_data;
       double recieved_array[BUFF_SIZE_RECEIVER];
       if (-1 == msgrcv(msqid_reciever, &recieve_data, sizeof(t_recieve_double_data) - sizeof(long), 0, 0)) {
-        printf("here\n")
+        printf("here\n");
         perror( "msgrcv() failed");
         exit(1);
       }
