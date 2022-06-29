fd7cda7939282d21083ce11527ebb12ac472b46f
sjmluo <simonjluo@gmail.com>
2022-06-29 17:51:51 +1000

    sending map size

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -10,6 +10,7 @@ class RLFuzzing:
     def __init__(self,max_message_size=10000):
         self.mq_reciever = sysv_ipc.MessageQueue(1, sysv_ipc.IPC_CREAT, max_message_size=max_message_size)
         self.mq_sender = sysv_ipc.MessageQueue(2, sysv_ipc.IPC_CREAT, max_message_size=max_message_size)
+        self.step_exec_map = None
         return
 
     def recieve_messages(self, BUFF_SIZE_RECIEVER=1024):
@@ -32,9 +33,15 @@ class RLFuzzing:
                     message_numpy_array = np.frombuffer(message, dtype=np.uintc)
                     trace_bits = np.concatenate([trace_bits, message_numpy_array[1:map_size]])
                 trace_bits = trace_bits[:map_size]
+
+                if self.step_exec_map is None:
+                    self.step_exec_map = np.zeros(map_size)
+
+                self.step_exec_map[trace_bits != 0] += 1
                 print(f"afl->fsrv.map_size: {map_size}")
                 print(f"afl->fsrv.trace_bits: {trace_bits}")
                 print(f"len(trace_bits): {len(trace_bits)}")
+                print(f"self.step_exec_map: ", {self.step_exec_map})
                 print(f"mtype: {mtype}")
                 # self.send_messenges(mtype)
 
