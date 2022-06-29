a9a16b19214e6bcb4bd1850bc99a4ae1ca9d16e1
sjmluo <simonjluo@gmail.com>
2022-06-29 14:28:55 +1000

    sending map size

--- include/afl-fuzz.h
+++ include/afl-fuzz.h
@@ -84,8 +84,8 @@
   #include <sys/ipc.h>
   #include <sys/msg.h>
 
-  #define  BUFF_SIZE_SENDER     4294967296
-  #define  BUFF_SIZE_RECEIVER   4294967296
+  #define  BUFF_SIZE_SENDER     1048576
+  #define  BUFF_SIZE_RECEIVER   1048576
 
 
   typedef struct {
