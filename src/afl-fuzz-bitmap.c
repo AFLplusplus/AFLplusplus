bc8dd534177fbaf7a5653e5607f889f1740f5ce1
sjmluo <simonjluo@gmail.com>
2022-06-29 14:24:08 +1000

    sending map size

--- src/afl-fuzz-bitmap.c
+++ src/afl-fuzz-bitmap.c
@@ -583,7 +583,8 @@ save_if_interesting(afl_state_t *afl, void *mem, u32 len, u8 fault) {
     u32 msg_array[BUFF_SIZE_SENDER];
     msg_array[0] = afl->fsrv.map_size;
     for (u32 i = 0; i < afl->fsrv.map_size; i++) {
-      msg_array[i+1] = (u32) afl->fsrv.trace_bits[i];
+      // msg_array[i+1] = (u32) afl->fsrv.trace_bits[i];
+      msg_array[i+1] = (u32) 1;
     }
     memcpy(send_data.data_buff, msg_array, BUFF_SIZE_SENDER * sizeof(u32));
     if (-1 == msgsnd(msqid_sender, &send_data, sizeof(t_send_u32_data) - sizeof(long), 0)) {
