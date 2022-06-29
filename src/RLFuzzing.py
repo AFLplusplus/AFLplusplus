51bbce1a3cf2a6d04087908dad92790b79e4b05b
sjmluo <simonjluo@gmail.com>
2022-06-29 17:43:56 +1000

    sending map size

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -31,12 +31,10 @@ class RLFuzzing:
                 while len(trace_bits) < map_size:
                     message_numpy_array = np.frombuffer(message, dtype=np.uintc)
                     trace_bits = np.concatenate([trace_bits, message_numpy_array[1:map_size]])
-                tmp = trace_bits[map_size:]
                 trace_bits = trace_bits[:map_size]
                 print(f"afl->fsrv.map_size: {map_size}")
                 print(f"afl->fsrv.trace_bits: {trace_bits}")
                 print(f"len(trace_bits): {len(trace_bits)}")
-                print(f"tmp: {tmp}")
                 print(f"mtype: {mtype}")
                 # self.send_messenges(mtype)
 
