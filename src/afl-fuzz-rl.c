70068d9a471cb2655fc2e04bed366549f162af5c
sjmluo <simonjluo@gmail.com>
2022-07-08 22:21:07 +1000

    added RL struct for batch update

--- src/afl-fuzz-rl.c
+++ src/afl-fuzz-rl.c
@@ -115,8 +115,7 @@ void update_queue(rl_params_t *rl_params) {
   t_u32_data send_data;
   send_data.data_type = 1;
   u32 msg_array[BUFF_SIZE];
-  msg_array[0] = map_size;
-  OKF("map size %d", map_size);
+  msg_array[0] = rl_params->map_size;
 
   memcpy(send_data.data_buff, msg_array, BUFF_SIZE * sizeof(u32));
   if (-1 == msgsnd(rl_params->msqid_sender, &send_data, sizeof(t_u32_data) - sizeof(long), 0)) {
