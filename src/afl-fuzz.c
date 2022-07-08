7c077129cdc105d3b12beaf6c16209b5042a7f51
sjmluo <simonjluo@gmail.com>
2022-07-08 17:06:04 +1000

    added RL struct for batch update

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -2269,7 +2269,7 @@ int main(int argc, char **argv_orig, char **envp) {
 
   while (likely(!afl->stop_soon)) {
     if (RLFUZZING) {
-      afl->rl_params.map_size = afl->fsrv.map_size;
+      afl->rl_params.map_size = afl->fsrv->map_size;
       afl->rl_params->queue_cur = afl->queue_cur;
       afl->rl_params->top_rated = afl->top_rated;
       update_queue(afl->rl_params);
