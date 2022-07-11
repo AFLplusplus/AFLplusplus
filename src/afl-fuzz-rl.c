c776d5ba5296d88277f80e9e7ff6610c9b884898
sjmluo <simonjluo@gmail.com>
2022-07-11 16:06:44 +1000

    cleanup code

--- src/afl-fuzz-rl.c
+++ src/afl-fuzz-rl.c
@@ -26,7 +26,6 @@ rl_params_t* init_rl_params(u32 map_size){
   send_data.data_type = 1;
   u32 msg_array[BUFF_SIZE];
   msg_array[0] = map_size;
-  OKF("map size %d", map_size);
 
   memcpy(send_data.data_buff, msg_array, BUFF_SIZE * sizeof(u32));
   if (-1 == msgsnd(rl_params->msqid_sender, &send_data, sizeof(t_u32_data) - sizeof(long), 0)) {
@@ -48,62 +47,6 @@ void store_features(rl_params_t *rl_params) {
             rl_params->negative_reward[i] += 1;
         }
     }
-
-
-    // int msqid_sender;
-    // int msqid_reciever;
-
-    // if (-1 == ( msqid_sender = msgget( (key_t)1, IPC_CREAT | 0666))) {
-    //   perror("msgget() failed");
-    //   exit(1);
-    // }
-
-    // if (-1 == ( msqid_reciever = msgget( (key_t)2, IPC_CREAT | 0666))) {
-    //   perror("msgget() failed");
-    //   exit(1);
-    // }
-
-
-    // /* Send Messages */
-    // t_send_u32_data send_data;
-    // send_data.data_type = 2;
-    // u32 msg_array[BUFF_SIZE_SENDER];
-
-    // u32 index = 0;
-    // while (index < rl_params.map_size) {
-    //   msg_array[0] = rl_params.map_size;
-      
-    //   for (u32 i = 0; i < (BUFF_SIZE_SENDER - 1); i++) {
-    //     if (index+i < rl_params.map_size) {
-    //       msg_array[i+1] = (u32) rl_params.trace_bits[index+i];
-    //     } else {
-    //        msg_array[i+1] = (u32) 0;
-    //     }
-    //   }
-    //   memcpy(send_data.data_buff, msg_array, BUFF_SIZE_SENDER * sizeof(u32));
-    //   if (-1 == msgsnd(msqid_sender, &send_data, sizeof(t_send_u32_data) - sizeof(long), 0)) {
-    //     perror("msgsnd() failed");
-    //     exit(1);
-    //   }
-    //   index += BUFF_SIZE_SENDER;
-    // }
-
-
-  // /* Receive Messages */
-  //   t_recieve_u8_data recieve_data;
-  //   u8 recieved_array[BUFF_SIZE_RECEIVER];
-  //   if (-1 == msgrcv(msqid_reciever, &recieve_data, sizeof(t_recieve_u8_data) - sizeof(long), 0, 0)) {
-  //     perror( "msgrcv() failed");
-  //     exit(1);
-  //   }
-
-  //   memcpy(recieved_array, recieve_data.data_buff, BUFF_SIZE_RECEIVER * sizeof(double));
-  //   printf("Interpreted as array: ");
-  //   for(int i = 0; i<BUFF_SIZE_RECEIVER; i++) {
-  //     printf("%d ", recieved_array[i]);
-  //   }
-  //   printf("\n");
-
 }
 
 void update_queue(rl_params_t *rl_params) {
@@ -169,47 +112,22 @@ void update_queue(rl_params_t *rl_params) {
     index += BUFF_SIZE;
   }
 
+  /* Receive Messages */
+  t_u32_data recieve_data;
+  u32 recieved_array[BUFF_SIZE];
+  if (-1 == msgrcv(rl_params->msqid_reciever, &recieve_data, sizeof(t_u32_data) - sizeof(long), 0, 0)) {
+    perror( "msgrcv() failed");
+    exit(1);
+  }
 
+  memcpy(recieved_array, recieve_data.data_buff, BUFF_SIZE * sizeof(u32));
 
-  // /* Receive Messages */
-  // t_recieve_double_data recieve_data;
-  // double recieved_array[BUFF_SIZE_RECEIVER];
-  // double score_array[afl->fsrv.map_size];
-  // u32 index = 0;
-  // while (index < afl->fsrv.map_size) {
-  //   if (-1 == msgrcv(msqid_reciever, &recieve_data, sizeof(t_recieve_double_data) - sizeof(long), 0, 0)) {
-  //     perror( "msgrcv() failed");
-  //     exit(1);
-  //   }
-  //   memcpy(recieved_array, recieve_data.data_buff, BUFF_SIZE_RECEIVER * sizeof(double));
-  //   for(u32 i = 0; i < BUFF_SIZE_RECEIVER; i++) {
-  //     if(index+i < afl->fsrv.map_size) {
-  //       score_array[index+i] = recieved_array[i];
-  //     }
-  //   }
-  //   index += BUFF_SIZE_RECEIVER;
-  // }
-  // (void)score_array; // Silence Error Remove Later
+  rl_params->current_entry = (u32) recieved_array[0];
 
-
-    /* Receive Messages */
-    t_u32_data recieve_data;
-    u32 recieved_array[BUFF_SIZE];
-    if (-1 == msgrcv(rl_params->msqid_reciever, &recieve_data, sizeof(t_u32_data) - sizeof(long), 0, 0)) {
-      perror( "msgrcv() failed");
-      exit(1);
-    }
-
-    memcpy(recieved_array, recieve_data.data_buff, BUFF_SIZE * sizeof(u32));
-
-    rl_params->current_entry = (u32) recieved_array[0];
-
-    OKF("Modifying queue with RL\n");
-    rl_params->queue_cur = rl_params->top_rated[(int) rl_params->current_entry];
-    if (rl_params->queue_cur) {
-      OKF("Setting queue_cur to %d", rl_params->queue_cur->id);
-      rl_params->current_entry = rl_params->queue_cur->id;
-    }
+  rl_params->queue_cur = rl_params->top_rated[(int) rl_params->current_entry];
+  if (rl_params->queue_cur) {
+    rl_params->current_entry = rl_params->queue_cur->id;
+  }
 
 
 
