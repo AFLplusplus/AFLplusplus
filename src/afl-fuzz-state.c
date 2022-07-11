6dc7070cb24eabe904a4770550a4a7808a044dac
sjmluo <simonjluo@gmail.com>
2022-07-11 18:29:06 +1000

    cleanup code

--- src/afl-fuzz-state.c
+++ src/afl-fuzz-state.c
@@ -140,6 +140,7 @@ void afl_state_init(afl_state_t *afl, uint32_t map_size) {
 
   list_append(&afl_states, afl);
 
+  afl->rl_params = init_rl_params(afl->fsrv.map_size); //TODO SET THIS AS AN ENVIRONMENT VARIABLE IN THE FUTURE! I HAVE JUST PUT THIS HERE FOR CONVIENENCE
 
 
 }
@@ -148,7 +149,6 @@ void afl_state_init(afl_state_t *afl, uint32_t map_size) {
  * struct*/
 
 void read_afl_environment(afl_state_t *afl, char **envp) {
-  afl->rl_params = init_rl_params(afl->fsrv.map_size); //TODO SET THIS AS AN ENVIRONMENT VARIABLE IN THE FUTURE! I HAVE JUST PUT THIS HERE FOR CONVIENENCE
 
   int   index = 0, issue_detected = 0;
   char *env;
