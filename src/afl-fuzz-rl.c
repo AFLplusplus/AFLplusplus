4ed9736e0d82c082796b19f86739ffff5ac77161
sjmluo <simonjluo@gmail.com>
2022-07-12 10:25:03 +1000

    cleanup code

--- src/afl-fuzz-rl.c
+++ src/afl-fuzz-rl.c
@@ -40,7 +40,7 @@ rl_params_t* init_rl_params(u32 map_size){
 
 void store_features(rl_params_t *rl_params) {
   u8 *trace_bits = rl_params->trace_bits;
-  u32 trace_bits_len = (u32)(sizeof(trace_bits) / sizeof(u8))
+  u32 trace_bits_len = (u32)(sizeof(trace_bits) / sizeof(u8));
   for(u32 i = 0; i < trace_bits_len; i++) {
     if (trace_bits[i]) {
         rl_params->positive_reward[i] += 1;
