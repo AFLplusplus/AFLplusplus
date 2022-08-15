2462c61df9cffa5e29433913a73095d212b70403
vanhauser-thc <vh@thc.org>
2022-08-15 19:22:46 +0200

    fix new map size extraction for afl-gcc

--- afl-cmin.bash
+++ afl-cmin.bash
@@ -215,10 +215,13 @@ if [ ! -f "$TARGET_BIN" -o ! -x "$TARGET_BIN" ]; then
 
 fi
 
-MAPSIZE=`AFL_DUMP_MAP_SIZE=1 "./$TARGET_BIN" 2>/dev/null`
-test -n "$MAPSIZE" && {
-  export AFL_MAP_SIZE=$MAPSIZE
-  echo "[+] Setting AFL_MAP_SIZE=$MAPSIZE"
+grep -aq AFL_DUMP_MAP_SIZE "./$TARGET_BIN" && {
+  echo "[!] Trying to obtain the map size of the target ..."
+  MAPSIZE=`AFL_DUMP_MAP_SIZE=1 "./$TARGET_BIN" 2>/dev/null`
+  test -n "$MAPSIZE" && {
+    export AFL_MAP_SIZE=$MAPSIZE
+    echo "[+] Setting AFL_MAP_SIZE=$MAPSIZE"
+  }
 }
 
 if [ "$AFL_SKIP_BIN_CHECK" = "" -a "$QEMU_MODE" = "" -a "$FRIDA_MODE" = "" -a "$UNICORN_MODE" = "" ]; then
