143f3e5225a2f6e83165872b62e9a351a75ce9d3
Simon Luo <simon.luo@unsw.edu.au>
2022-08-23 14:30:44 +1000

    added rare edges

--- src/RLFuzzing.py
+++ src/RLFuzzing.py
@@ -21,6 +21,7 @@ class CorrectionFactor(Enum):
     WITHOUT_SQUARE_ROOT = 'without_square_root'
     WITH_SQUARE_ROOT = 'with_square_root'
     SAMPLE = 'sample'
+    RARE = 'rare'
 
     def __str__(self):
         return self.value
@@ -69,6 +70,9 @@ class RLFuzzing:
                         (pos_reward**2 + pos_reward + neg_reward))**0.5
         elif self.correction_factor is CorrectionFactor.SAMPLE:
             rareness = np.random.beta(pos_reward + neg_reward, pos_reward**2)
+        elif self.correction_factor is CorrectionFactor.RARE:
+            rareness = np.random.beta(pos_reward + neg_reward, pos_reward**2)
+            return rareness
 
         return random_beta * rareness
 
