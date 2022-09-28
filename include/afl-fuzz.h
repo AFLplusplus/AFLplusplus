f87e40c0652a3559a51105b6a47bc2cee65163d7
sjmluo <simonjluo@gmail.com>
2022-09-28 18:37:17 +1000

    hard coded overhead computation

--- include/afl-fuzz.h
+++ include/afl-fuzz.h
@@ -84,6 +84,15 @@
 
 #define CALCULATE_OVERHEAD 1
 
+#ifdef CALCULATE_OVERHEAD
+  typedef unsigned long long timestamp_t;
+
+  static timestamp_t get_timestamp () {
+    struct timeval now;
+    gettimeofday (&now, NULL);
+    return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+  }
+#endif
 /* End RL fuzzing */
 
 #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || \
