2cb2eda5e491a2d6661228a6ee2808a76b5f44d2
Maciej Domanski <maciej.domanski@trailofbits.com>
2022-12-27 16:15:52 +0100

    update

--- utils/argv_fuzzing/argv_fuzz_demo.c
+++ utils/argv_fuzzing/argv_fuzz_demo.c
@@ -3,7 +3,14 @@
 #include "argv-fuzz-inl.h"
 
 int main(int argc, char **argv) {
-AFL_INIT_ARGV();
+  // Initialize the argv array for use with the AFL (American Fuzzy Lop) tool
+  AFL_INIT_ARGV();
+
+  /* Check the number of command line arguments and
+    compare the values of the first two arguments to specific strings.
+    If the number of arguments is not correct or the values do not match,
+    an error message is printed. If the values do match, the program
+    calls the abort() function. */
   if (argc > 1 && strcmp(argv[1], "XYZ") == 0) {
     if (strcmp(argv[2], "TEST2") == 0) {
       abort();
