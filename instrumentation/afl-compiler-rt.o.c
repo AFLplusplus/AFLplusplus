5f6b603898bef756fd52f630d0786c41520b738a
vanhauser-thc <vh@thc.org>
2023-01-03 09:38:00 +0100

    welcome 2023

--- instrumentation/afl-compiler-rt.o.c
+++ instrumentation/afl-compiler-rt.o.c
@@ -3,7 +3,7 @@
    ------------------------------------------------
 
    Copyright 2015, 2016 Google Inc. All rights reserved.
-   Copyright 2019-2022 AFLplusplus Project. All rights reserved.
+   Copyright 2019-2023 AFLplusplus Project. All rights reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
