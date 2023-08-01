92a9bfea5103a6f9279b644a851768e6b43101d6
Junwha <qbit@unist.ac.kr>
2023-08-02 02:59:07 +0900

    Add option for treating crashing input as new crash
    Signed-off-by: Junwha Hong <qbit@unist.ac.kr>

--- include/afl-fuzz.h
+++ include/afl-fuzz.h
@@ -539,7 +539,8 @@ typedef struct afl_state {
       expand_havoc,                /* perform expensive havoc after no find */
       cycle_schedules,                  /* cycle power schedules?           */
       old_seed_selection,               /* use vanilla afl seed selection   */
-      reinit_table;                     /* reinit the queue weight table    */
+      reinit_table,                     /* reinit the queue weight table    */
+      crashing_seeds_as_new_crash; /* treat crashing seeds as normal corpus */
 
   u8 *virgin_bits,                      /* Regions yet untouched by fuzzing */
       *virgin_tmout,                    /* Bits we haven't seen in tmouts   */
