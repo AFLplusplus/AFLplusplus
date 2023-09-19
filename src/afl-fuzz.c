6dc054be0faa2fe934ec24465f75fb8676798d13
Thomas Rooijakkers <thomas.rooijakkers@tno.nl>
2023-09-19 13:42:53 +0200

    Add usage compilation status print for NO_UTF

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -381,6 +381,12 @@ static void usage(u8 *argv0, int more_help) {
   SAYF("Compiled with NO_SPLICING.\n");
 #endif
 
+#ifdef NO_UTF
+  SAYF("Compiled without UTF-8 support for line rendering in status screen.\n");
+#else
+  SAYF("Compiled with UTF-8 support for line rendering in status screen.\n");
+#endif
+
 #ifdef PROFILING
   SAYF("Compiled with PROFILING.\n");
 #endif
