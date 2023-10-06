7dec7fb538465974ee4c94db22dfd35d044e45ef
toka <tokazerkje@outlook.com>
2023-10-06 16:51:34 +0200

    insert coverage pass BEFORE optimizations

--- instrumentation/SanitizerCoveragePCGUARD.so.cc
+++ instrumentation/SanitizerCoveragePCGUARD.so.cc
@@ -214,7 +214,7 @@ llvmGetPassPluginInfo() {
 #if LLVM_VERSION_MAJOR == 13
             using OptimizationLevel = typename PassBuilder::OptimizationLevel;
 #endif
-            PB.registerOptimizerLastEPCallback(
+            PB.registerOptimizerEarlyEPCallback(
                 [](ModulePassManager &MPM, OptimizationLevel OL) {
 
                   MPM.addPass(ModuleSanitizerCoverageAFL());
