c5303bb2a130dc293b2159a9e281f25b2806588f
Han Zheng <han.zheng@epfl.ch>
2024-01-29 18:18:54 +0100

    revert fix

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -1426,10 +1426,9 @@ int main(int argc, char **argv_orig, char **envp) {
   #endif
   if (!afl->skip_deterministic && afl->afl_env.afl_custom_mutator_only) {
 
-    afl->skip_deterministic = 1;
-    WARNF(
+    FATAL(
         "Using -D determinstic fuzzing is incompatible with "
-        "AFL_CUSTOM_MUTATOR_ONLY, now we disable skip deterministic!");
+        "AFL_CUSTOM_MUTATOR_ONLY!");
 
   }
 
