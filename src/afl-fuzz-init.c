64b642750f10374006ece94c7a35e7b726ef6221
mio <mio@lazym.io>
2025-01-26 15:34:56 +0800

    Fix plot_file header

--- src/afl-fuzz-init.c
+++ src/afl-fuzz-init.c
@@ -2332,7 +2332,15 @@ void setup_dirs_fds(afl_state_t *afl) {
         afl->fsrv.plot_file,
         "# relative_time, cycles_done, cur_item, corpus_count, "
         "pending_total, pending_favs, map_size, saved_crashes, "
-        "saved_hangs, max_depth, execs_per_sec, total_execs, edges_found, total_crashes, servers_count, san1_exec...\n");
+        "saved_hangs, max_depth, execs_per_sec, total_execs, edges_found, total_crashes, servers_count");
+
+    if (afl->san_binary_length) {
+      for (u8 i = 0; i < afl->san_binary_length; i++) {
+        fprintf(afl->fsrv.plot_file, ", sand_fsrv%u_exec", i);
+      }
+    }
+
+    fprintf(afl->fsrv.plot_file, "\n");
 
   } else {
 
