1cd84007c07a756a6f1d82a6b474efd807648289
w1redch4d <weirdflexbutok200@gmail.com>
2025-04-10 19:44:54 +0530

    added safe_length option for portability and clarity

--- include/android-ashmem.h
+++ include/android-ashmem.h
@@ -30,8 +30,9 @@ int shmctl(int __shmid, int __cmd, struct shmid_ds *__buf) {
   int ret = 0;
   if (__cmd == IPC_RMID) {
 
-    unsigned int length = ioctl(__shmid, ASHMEM_GET_SIZE, NULL);
-    struct ashmem_pin pin = {0, length};
+    int length = ioctl(__shmid, ASHMEM_GET_SIZE, NULL);
+    unsigned int safe_length = length >= 0 ? length : 0;
+    struct ashmem_pin pin = {0, safe_length};
     ret = ioctl(__shmid, ASHMEM_UNPIN, &pin);
     close(__shmid);
 
