bc7e83dfa6d4be1fb3b4766a430c2c307165aa2e
toka <tokazerkje@outlook.com>
2025-07-11 15:30:22 +0200

    mm

--- src/afl-fuzz-skipdet.c
+++ src/afl-fuzz-skipdet.c
@@ -159,12 +159,17 @@ u8 skip_deterministic_stage(afl_state_t *afl, u8 *orig_buf, u8 *out_buf,
         for(int y = 0; y < 10; y++) {
           printf("Repetition %llu\n", y);
           char filename[64];
+          char inputname[64];
           snprintf(filename, sizeof(filename), "file_%d_%d_%d.bin", pos, cur_block_size, y);
+          snprintf(inputname, sizeof(inputname), "input_%d_%d_%d.bin", pos, cur_block_size, y);
 
           if (common_fuzz_stuff(afl, out_buf, len)) return 0;\
-          FILE* fp = fopen(filename, "wb"); // Open file in binary write mode
-          fwrite(afl->fsrv.trace_bits, 1, afl->fsrv.map_size, fp);
-          fclose(fp);
+          FILE* fp1 = fopen(filename, "wb"); // Open file in binary write mode
+          FILE* fp2 = fopen(inputname, "wb"); // Open file in binary write mode
+          fwrite(afl->fsrv.trace_bits, 1, afl->fsrv.map_size, fp1);
+          fwrite(out_buf, 1, len, fp2);
+          fclose(fp1);
+          fclose(fp2);
           cksum =
             hash64(afl->fsrv.trace_bits, afl->fsrv.map_size, HASH_CONST);
           printf("cksum %d\n", cksum);
