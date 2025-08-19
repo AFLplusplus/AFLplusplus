987d08281f32550e2739bb1cb38b07113a712e4a
vanhauser-thc <vh@thc.org>
2025-08-19 09:53:36 +0200

    fix smallest favored

--- src/afl-fuzz-queue.c
+++ src/afl-fuzz-queue.c
@@ -972,7 +972,8 @@ void cull_queue(afl_state_t *afl) {
         if (!afl->top_rated[i]->was_fuzzed) {
 
           ++afl->pending_favored;
-          if (unlikely(afl->smallest_favored < 0)) {
+          if (unlikely(afl->smallest_favored == -1 ||
+                       afl->smallest_favored > (s64)afl->top_rated[i]->id)) {
 
             afl->smallest_favored = (s64)afl->top_rated[i]->id;
 
