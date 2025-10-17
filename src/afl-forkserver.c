5bc2b5c09cb61a7c21be49cee50a1fefccdd6037
trowlett0 <tannerrowlett03@gmail.com>
2025-10-16 18:46:16 -0600

    Add GUI mode logic to afl_fauxsrv_execv

--- src/afl-forkserver.c
+++ src/afl-forkserver.c
@@ -559,6 +559,29 @@ static void afl_fauxsrv_execv(afl_forkserver_t *fsrv, char **argv) {
 
       }
 
+      /* GUI mode */
+
+      #ifdef __linux__
+        if(unlikely(fsrv->gui_mode)) {
+          pid_t python_pid;
+          pid_t parent_pid = getpid(); // Get the current PID of this soon to be GUI process
+          python_pid = fork();
+
+          if (python_pid < 0) {PFATAL("GUI mode fork failed.");}
+
+          if (!python_pid) { // New python interactor
+            ACTF("Non-forkserver exec'ing, with PID = %ld\n", (long) parent_pid);
+            char parent_pid_str[16];
+            sprintf(parent_pid_str, "%d", (int)parent_pid); // Convert pid_t to a string
+            
+            char *pargs[] = {"/usr/bin/python3", fsrv->gui_python_dir, "-o", fsrv->out_file, "-p", parent_pid_str, NULL};
+            execv("/usr/bin/python3", pargs);
+
+            exit(0);
+          }
+        }
+      #endif
+
       // finally: exec...
       execv(fsrv->target_path, argv);
 
