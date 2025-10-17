8a09c5e5badbcc345b35b23d12a8570ff161a976
trowlett0 <tannerrowlett03@gmail.com>
2025-10-16 18:18:39 -0600

    Fix formatting

--- include/forkserver.h
+++ include/forkserver.h
@@ -235,8 +235,8 @@ typedef struct afl_forkserver {
   u64                   nyx_target_hash64;
 
   bool                  gui_mode;        /* if running in gui mode or not    */
-  s32                   *python_pid;     /* PID of python interactor         */
-  u8                    *gui_py_dir;     /* location of python interactor    */
+  s32                  *python_pid;      /* PID of python interactor         */
+  u8                   *gui_py_dir;      /* location of python interactor    */
 
 #endif
 
