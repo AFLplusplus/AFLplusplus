63424b0702177d43d3fa07e11b01755ca00e53a6
loqs <loqs@users.noreply.github.com>
2026-01-14 02:58:09 +0000

    unicornafl: Detect setuptools using import (#2654)
    Fixes https://github.com/AFLplusplus/AFLplusplus/issues/2651

--- unicorn_mode/build_unicorn_support.py
+++ unicorn_mode/build_unicorn_support.py
@@ -116,7 +116,9 @@ elif not venv and skip_venv:
 
 print(f"[*] We will install unicornafl to venv at {venv_prefix} using {py3}")
 
-if not shutil.which("setuptools"):
+try:
+  run_cmd(f"{py3} -c 'import setuptools'")
+except subprocess.CalledProcessError:
     print(f"[!] No setuptools, will install setuptools first")
     if skip_venv:
         run_cmd(f"{py3} -m pip install --user setuptools")
