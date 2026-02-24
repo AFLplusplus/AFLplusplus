1aca4113c255629c1685ea6c76a23398479784e2
Khaled Yakdan <yakdan@code-intelligence.com>
2026-02-24 10:08:46 +0100

    tests: extend value profile unit coverage for l1 frontier
    Expand VP unit tests around L1 behavior:
    
    - add stubs needed by `afl-fuzz-valprof.o` test linkage
    - add frontier update/overflow and solved-slot replacement cases
    - factor frontier setup/teardown helpers for clearer test intent

--- test/unittests/unit_value_profile.c
+++ test/unittests/unit_value_profile.c
@@ -5,21 +5,44 @@
 #include <cmocka.h>
 /* cmocka < 1.0 didn't support these features we need */
 #ifndef assert_ptr_equal
-#define assert_ptr_equal(a, b)                                                  \
-  _assert_int_equal(cast_ptr_to_largest_integral_type(a),                      \
-                    cast_ptr_to_largest_integral_type(b), __FILE__, __LINE__)
-#define CMUnitTest UnitTest
-#define cmocka_unit_test unit_test
-#define cmocka_run_group_tests(t, setup, teardown) run_tests(t)
+  #define assert_ptr_equal(a, b)                                      \
+    _assert_int_equal(cast_ptr_to_largest_integral_type(a),           \
+                      cast_ptr_to_largest_integral_type(b), __FILE__, \
+                      __LINE__)
+  #define CMUnitTest UnitTest
+  #define cmocka_unit_test unit_test
+  #define cmocka_run_group_tests(t, setup, teardown) run_tests(t)
 #endif
 
 extern void mock_assert(const int result, const char *const expression,
                         const char *const file, const int line);
 #undef assert
-#define assert(expression)                                                       \
+#define assert(expression) \
   mock_assert((int)(expression), #expression, __FILE__, __LINE__);
 
 #include "afl-fuzz.h"
+#include "value-profile.h"
+
+/* Stubs for functions referenced by afl-fuzz-valprof.o. */
+u32 write_to_testcase(afl_state_t *afl, void **mem, u32 len, u32 fix) {
+
+  (void)afl;
+  (void)mem;
+  (void)fix;
+  return len;
+
+}
+
+
+fsrv_run_result_t fuzz_run_target(afl_state_t *afl, afl_forkserver_t *fsrv,
+                                  u32 timeout) {
+
+  (void)afl;
+  (void)fsrv;
+  (void)timeout;
+  return FSRV_RUN_OK;
+
+}
 
 /* remap exit -> assert, then use cmocka's mock_assert
    (compile with `--wrap=exit`) */
@@ -106,6 +129,9 @@ static void test_non_stagnation_mode_is_noop(void **state) {
 
 }
 
+static void setup_vp_frontier(afl_state_t *afl, u32 slots);
+static void free_vp_frontier(afl_state_t *afl);
+
 static void test_wide_ins_compare_keeps_vp_site_active(void **state) {
 
   (void)state;
@@ -122,15 +148,14 @@ static void test_wide_ins_compare_keeps_vp_site_active(void **state) {
   cmp = calloc(1, sizeof(struct cmp_map));
   assert_non_null(cmp);
   afl.shm.cmp_map = cmp;
+  afl.value_profile_level = 2;
+  afl.value_profile_source = VP_SOURCE_CMPLOG_INLINE;
 
-  afl.top_rated_vp = calloc(CMP_MAP_W, sizeof(struct queue_entry *));
-  assert_non_null(afl.top_rated_vp);
-  afl.top_rated_vp_dist = calloc(CMP_MAP_W, sizeof(u32));
-  assert_non_null(afl.top_rated_vp_dist);
-  memset(afl.top_rated_vp_dist, 0xff, CMP_MAP_W * sizeof(u32));
+  setup_vp_frontier(&afl, 1);
 
   old_q.exec_us = 100;
   old_q.len = 100;
+  old_q.vp_ref_cnt = 1;
   new_q.exec_us = 1;
   new_q.len = 1;
 
@@ -142,25 +167,31 @@ static void test_wide_ins_compare_keeps_vp_site_active(void **state) {
   cmp->log[0][0].v1 = 0x1122334455667788ULL;
   cmp->log[0][0].v0_128 = 0x1ULL;
   cmp->log[0][0].v1_128 = 0x2ULL;
+  afl.vp_trigger_bitmap[0] = 1;
 
-  afl.vp_trigger_bitmap[0] = 1ULL;
+  afl.vp_frontier[0].owner = &old_q;
+  afl.vp_frontier[0].dist = 10;
+  afl.vp_frontier[0].tag = 0;
+  afl.vp_frontier[0].cost = 10000;
   afl.top_rated_vp[0] = &old_q;
   afl.top_rated_vp_dist[0] = 10;
 
-  vp_update_bitmap_score(&afl, &new_q);
+  vp_frontier_apply(&afl, &new_q);
 
   vp_saved = afl.top_rated_vp[0];
   assert_ptr_equal(vp_saved, &new_q);
+  assert_int_equal(old_q.vp_ref_cnt, 0);
+  assert_true(new_q.vp_ref_cnt > 0);
   assert_true(afl.top_rated_vp_dist[0] < 65);
   assert_true(afl.top_rated_vp_dist[0] != 0xffffffff);
 
-  free(afl.top_rated_vp_dist);
-  free(afl.top_rated_vp);
+  free_vp_frontier(&afl);
   free(cmp);
 
 }
 
-static void test_solved_wide_ins_compare_does_not_consume_vp_bits(void **state) {
+static void test_solved_wide_ins_compare_does_not_consume_vp_bits(
+    void **state) {
 
   (void)state;
 
@@ -235,6 +266,83 @@ static void test_solved_rtn_compare_does_not_consume_vp_bits(void **state) {
 
 }
 
+static void setup_vp_frontier(afl_state_t *afl, u32 slots) {
+
+  size_t n = (size_t)CMP_MAP_W * slots;
+  afl->value_profile_level = 1;
+  afl->value_profile_slots = slots;
+  afl->top_rated_vp = calloc(CMP_MAP_W, sizeof(struct queue_entry *));
+  assert_non_null(afl->top_rated_vp);
+  afl->top_rated_vp_dist = calloc(CMP_MAP_W, sizeof(u32));
+  assert_non_null(afl->top_rated_vp_dist);
+  for (u32 i = 0; i < CMP_MAP_W; ++i) {
+
+    afl->top_rated_vp_dist[i] = VP_DIST_UNSOLVED;
+
+  }
+
+  afl->vp_frontier = calloc(n, sizeof(vp_frontier_entry_t));
+  assert_non_null(afl->vp_frontier);
+  for (size_t i = 0; i < n; ++i) {
+
+    afl->vp_frontier[i].dist = VP_DIST_UNSOLVED;
+
+  }
+
+}
+
+static void free_vp_frontier(afl_state_t *afl) {
+
+  free(afl->vp_frontier);
+  free(afl->top_rated_vp_dist);
+  free(afl->top_rated_vp);
+
+}
+
+static void test_runtime_frontier_update_with_overflow_scan(void **state) {
+
+  (void)state;
+
+  afl_state_t        afl;
+  vp_map_t          *vp;
+  struct queue_entry q;
+
+  memset(&afl, 0, sizeof(afl));
+  memset(&q, 0, sizeof(q));
+  q.exec_us = 3;
+  q.len = 7;
+
+  vp = calloc(1, sizeof(vp_map_t));
+  assert_non_null(vp);
+
+  afl.value_profile_source = VP_SOURCE_RUNTIME_SHM;
+  afl.value_profile_mode = 1;
+  afl.value_profile_active = 1;
+  afl.queue_cycle = 1;
+  afl.shm.vp_map = vp;
+  setup_vp_frontier(&afl, 4);
+
+  vp->exec_id = 1;
+  vp->enabled = 1;
+  vp->control_len = 1;
+  vp->control[0] = 7;
+  vp->site[7].valid_mask = 1;
+  vp->site[7].touched_mask = 1;
+  vp->site[7].slots[0].slot_key = 3;
+  vp->site[7].slots[0].best_dist = 9;
+
+  assert_true(vp_frontier_would_improve(&afl));
+  vp_frontier_apply(&afl, &q);
+
+  assert_ptr_equal(afl.top_rated_vp[7], &q);
+  assert_int_equal(afl.top_rated_vp_dist[7], 9);
+  assert_true(q.vp_ref_cnt > 0);
+
+  free_vp_frontier(&afl);
+  free(vp);
+
+}
+
 int main(int argc, char **argv) {
 
   (void)argc;
@@ -246,7 +354,8 @@ int main(int argc, char **argv) {
       cmocka_unit_test(test_non_stagnation_mode_is_noop),
       cmocka_unit_test(test_wide_ins_compare_keeps_vp_site_active),
       cmocka_unit_test(test_solved_wide_ins_compare_does_not_consume_vp_bits),
-      cmocka_unit_test(test_solved_rtn_compare_does_not_consume_vp_bits)};
+      cmocka_unit_test(test_solved_rtn_compare_does_not_consume_vp_bits),
+      cmocka_unit_test(test_runtime_frontier_update_with_overflow_scan)};
 
   __real_exit(cmocka_run_group_tests(tests, NULL, NULL));
 
