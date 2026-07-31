06a0044df8181bf5a1b701f3ba9474afe6315a68
Khaled Yakdan <yakdan@code-intelligence.com>
2026-07-31 11:48:56 +0200

    test: cover value-profile frontier behavior

--- test/test-unittests.sh
+++ test/test-unittests.sh
@@ -5,7 +5,6 @@
 $ECHO "$BLUE[*] Execution cmocka Unit-Tests $GREY"
 unset AFL_CC
 make -C .. unit || CODE=1 INCOMPLETE=1 :
-rm -rf unittests/unit_hash unittests/unit_rand
+rm -rf unittests/unit_hash unittests/unit_rand unittests/unit_value_profile
 
 . ./test-post.sh
-
